b6900f32e94289e4804b5987d863956ea0938cdf
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-03 20:37:41 -0600

    add nix config for 32bit support

---
+++ .nixos/misc/32bit.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: {
+  hardware = {
+    opengl = {
+      driSupport32Bit = true;
+      extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+    };
+    pulseaudio = {
+      support32Bit = true;
+    };
+  };
+}
