34a843eb9b8118e5365867f1dfa880a38ed9e2a4
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-07 13:50:25 -0600

    flesh out user configs

--- .nixos/eduardo.nix
+++ .nixos/eduardo.nix
@@ -4,6 +4,7 @@
     description = "Eduardo Quiros";
     extraGroups = [ "wheel" "video" "audio" "networkmanager" ];
     home = "/home/eduardo";
+    initialHashedPassword = "$6$n3GzxIULPTTidjy$8TABYAaFlEo/I49XGq1WpUCxpIZzrbxddlAXudwisO7S4y2Xi1e7ZQtUp1b/F8HiXLFi.WUBEie.a9/R/ewoJ0";
     isNormalUser = true;
   };
 }
