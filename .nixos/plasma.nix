b5e71eaf520ea9d6789625606c11228b8600a391
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-07 16:54:55 -0600

    add browser integration to plasma

--- .nixos/plasma.nix
+++ .nixos/plasma.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: {
   imports = [ ./xserver.nix ];
+  nixpkgs.config = {
+    firefox.enablePlasmaBrowserIntegration = true;
+  };
   services.xserver = {
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
