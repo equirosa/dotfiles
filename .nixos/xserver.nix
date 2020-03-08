cc28eaec0d403923d14a1aeded6db36bc72ba344
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-07 20:09:25 -0600

    add sxhkd stuff

--- .nixos/xserver.nix
+++ .nixos/xserver.nix
@@ -8,4 +8,15 @@
       libinput.enable = true;
     };
   };
+  home-manager.users.eduardo = { pkgs, ... }: {
+    services.sxhkd = {
+      enable = true;
+      keybindings = {
+        "super + Return" = "$TERMINAL";
+        "super + e" = "$TERMINAL -e $MAIL";
+        "super + r" = "$TERMINAL -e $FILE";
+        "super + w" = "$BROWSER";
+      };
+    };
+  };
 }
