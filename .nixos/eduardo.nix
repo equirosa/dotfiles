001c8abd6868d7099104010a4a9338fc3389c3d3
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-11 19:13:21 -0600

    add croc and magic wormhole to personal profile

--- .nixos/eduardo.nix
+++ .nixos/eduardo.nix
@@ -17,7 +17,8 @@
     description = "Eduardo Quiros";
     extraGroups = [ "wheel" "video" "audio" "networkmanager" ];
     home = "/home/eduardo";
-    initialHashedPassword = "$6$n3GzxIULPTTidjy$8TABYAaFlEo/I49XGq1WpUCxpIZzrbxddlAXudwisO7S4y2Xi1e7ZQtUp1b/F8HiXLFi.WUBEie.a9/R/ewoJ0";
+    initialHashedPassword =
+      "$6$n3GzxIULPTTidjy$8TABYAaFlEo/I49XGq1WpUCxpIZzrbxddlAXudwisO7S4y2Xi1e7ZQtUp1b/F8HiXLFi.WUBEie.a9/R/ewoJ0";
     isNormalUser = true;
     packages = with pkgs; [
       aerc # Mail reader
@@ -26,6 +27,7 @@
       spaceFM # Graphical file manager
       texlive.combined.scheme-full # LaTeX stuff
       zathura # Doc viewer
+      croc magic-wormhole # Transfer stuff
 
       #### Nixpkgs stuff
       nix-prefetch-git
