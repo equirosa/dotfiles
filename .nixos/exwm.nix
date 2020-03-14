65480835ccda70a7a4ad982ff1f1a90c13c2a092
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-14 15:59:37 -0600

    add exwm

---
+++ .nixos/exwm.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ...}: {
+  imports = [ ./xserver.nix ];
+  services.xserver.windowManager.exwm = {
+    enable = true;
+    enableDefaultConfig = true;
+    extraPackages = epkgs: [
+      epkgs.emms
+      epkgs.magit
+      epkgs.nix-mode
+      epkgs.markdown-mode
+      epkgs.evil-mode
+    ];
+  };
+}
