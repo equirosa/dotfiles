dbc9eb1982e933da4af67d44616bee22653a9ba5
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-20 21:42:39 -0600

    comment out stuff from xserver config

--- .nixos/xserver.nix
+++ .nixos/xserver.nix
@@ -23,18 +23,20 @@
       libinput.enable = true;
     };
   };
-  #home-manager.users.eduardo = { pkgs, ... }: {
-    #services.sxhkd = {
-      #enable = true;
+  home-manager.users.eduardo = { pkgs, ... }: {
+    services.sxhkd = {
+      enable = true;
       #keybindings = {
         #"super + Return" = "$TERMINAL & disown";
         #"super + d" = "dmenu_run";
         #"super + e" = "$TERMINAL -e $MAIL";
         #"super + r" = "$TERMINAL -e $FILE";
         #"super + w" = "$BROWSER";
+        #"super + x" = "$LOCKSCREEN";
+        #"super + shift + p" = "emacsclient -p";
         #"super + q" =
-          #"xdotool windowkill $(xdotool getactivewindow)";
+          #'xdotool windowkill "$(xdotool getactivewindow)"';
       #};
-    #};
-  #};
+    };
+  };
 }
