ca1b976484cf91431abb59f0e88c1e1faa9e706c
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-20 18:16:35 -0600

    add autojump to bash

--- .nixos/bash.nix
+++ .nixos/bash.nix
@@ -2,6 +2,7 @@
   home-manager.users.eduardo = { pkgs, ... }: {
     programs.bash = {
       enable = true;
+      enableAutoJump = true;
       historyControl = [ "erasedups" ];
       historyIgnore = [ "ls" "cd" "exit" ];
     };
