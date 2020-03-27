20344e6e16bd2bacf61362caac29b2f28e6f4a30
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-26 19:15:35 -0600

    add a zsh config

---
+++ .nixos/shell/zsh.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }: {
+  home-manager.users.eduardo = { pkgs, ... }: {
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      autocd = true;
+      defaultKeymap = "vicmd";
+      dotDir = ".config/zsh";
+      history = {
+        ignoreDups = true;
+        path = ".cache/zsh_history";
+      };
+    };
+  };
+  users.users.eduardo.shell = pkgs.zsh;
+}
