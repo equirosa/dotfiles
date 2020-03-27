91b97d479b79151f41d1a442b65f9f6b6173dafe
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-26 23:51:26 -0600

    add correct escape characters to LESS env variables

--- .config/nixos/eduardo.nix
+++ .config/nixos/eduardo.nix
@@ -39,14 +39,14 @@
       nsearch = "nix-env -qaP --description";
     };
     variables = {
-            LESS = "-R";
-      LESS_TERMCAP_mb = "$(printf '%b' '')";
-      LESS_TERMCAP_md = "$(printf '%b' '')";
-      LESS_TERMCAP_me = "$(printf '%b' '')";
-      LESS_TERMCAP_so = "$(printf '%b' '')";
-      LESS_TERMCAP_se = "$(printf '%b' '')";
-      LESS_TERMCAP_us = "$(printf '%b' '')";
-      LESS_TERMCAP_ue = "$(printf '%b' '')";
+      LESS = "-R";
+      LESS_TERMCAP_mb = "$(printf '%b' '[1;31m')";
+      LESS_TERMCAP_md = "$(printf '%b' '[1;36m')";
+      LESS_TERMCAP_me = "$(printf '%b' '[0m')";
+      LESS_TERMCAP_so = "$(printf '%b' '[01;44;33m')";
+      LESS_TERMCAP_se = "$(printf '%b' '[0m')";
+      LESS_TERMCAP_us = "$(printf '%b' '[1;32m')";
+      LESS_TERMCAP_ue = "$(printf '%b' '[0m')";
     };
   };
   home-manager.users.eduardo = { pkgs, ... }: {
