e8313906df171d560e27813b0a6d76abdd1ea20e
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-28 15:12:54 -0600

    make bash source profile

--- .config/nixos/shell/bash.nix
+++ .config/nixos/shell/bash.nix
@@ -6,6 +6,9 @@
       historyControl = [ "erasedups" ];
       historyFile = "$HOME/.cache/bash_history";
       historyIgnore = [ "ls" "cd" "exit" ];
+      initExtra = ''
+        [ -f "$HOME/.profile" ] && source "$HOME/.profile"
+      '';
     };
   };
 }
