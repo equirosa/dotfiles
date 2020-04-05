53db1c4e5ef188f60f72ce17c0e8b114569c38cb
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-04 18:49:58 -0600

    remove unused nix configs

--- .config/nixos/next.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  environment.systemPackages = [ pkgs.next ];
-  home-manager.users.eduardo = { pkgs, ... }: {
-    home.file.nextConfig = {
-      source = ./config/next.lisp;
-      target = ".config/next/init.lisp";
-    };
-  };
-}
