fb36f5cf2277e460958a137ebeefd5d48a077bba
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-06 01:49:19 -0600

    add some color configs to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,2 +1,15 @@
+# imports
+import dracula.draw
+
 config.load_autoconfig()
+
+#Dracula Stuff
+dracula.draw.blood(c,{
+    'spacing':{
+        'vertical':6,
+        'horizontal':8
+    }
+})
+
+c.content.user_stylesheets = [ 'css/solarized_dark.css' ]
 config.set('content.javascript.enabled', False)
