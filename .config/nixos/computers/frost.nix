9db2e62399e82125701cdc0f2374762a6801c15b
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-05 20:45:31 -0600

    remove backup timers from frost

--- .config/nixos/computers/frost.nix
+++ .config/nixos/computers/frost.nix
@@ -8,49 +8,4 @@
     hostName = "frost";
     networkmanager.enable = true;
   };
-  programs.udevil.enable = true;
-  services = {
-    borgbackup.jobs = {
-      automatic-hourly = {
-        appendFailedSuffix = true;
-        compression = "auto,zstd,22";
-        doInit = false;
-        encryption = {
-          mode = "repokey-blake2";
-          passCommand = "cat /home/eduardo/.local/share/borgpassdisco";
-        };
-        exclude = [
-          "/home/eduardo/.config"
-          "*/.stfolder"
-          "*/.stversions"
-          "/home/eduardo/Games/league-of-legends/"
-        ];
-        paths = "/home/eduardo/";
-        prune.keep = {
-          within = "1d";
-          daily = 14;
-          weekly = 4;
-          monthly = 4;
-        };
-        removableDevice = true;
-        repo =
-          "/run/media/eduardo/2e571771-81db-41a5-a0b6-d5c6d3b8bf88/backups";
-        startAt = "hourly";
-        user = "eduardo";
-      };
-    };
-    duplicity = {
-      enable = true;
-      exclude = [
-        "/home/eduardo/.config"
-        "*/.stfolder"
-        "/home/eduardo/.emacs"
-        "*/.local"
-        "/home/eduardo/.bash*"
-      ];
-      root = "/home/eduardo";
-      secretFile = /home/eduardo/.local/share/duplicity_frost_pass;
-      targetUrl = "ftp://frost@eduardoquiros.com@c1560850.ferozo.com";
-    };
-  };
 }
