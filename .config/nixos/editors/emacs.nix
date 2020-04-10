966cbdd54c247b4be6a6fd03bacf25acece3a8af
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-10 15:15:07 -0600

    move emacs nix config to editor dir

--- .config/nixos/editors/emacs.nix
+++ .config/nixos/editors/emacs.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   services.emacs = {
     enable = true;
-    package = (import ./overrides/emacs.nix { pkgs = pkgs; });
+    package = (import ../overrides/emacs.nix { pkgs = pkgs; });
     defaultEditor = false;
   };
 }
