d273456dd617b378e879542815ad58f74957d960
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-10 03:46:09 -0600

    remove unused fonts

--- .config/nixos/fonts.nix
+++ .config/nixos/fonts.nix
@@ -5,19 +5,13 @@
       enable = true;
       defaultFonts = {
         monospace = [ "Fira Code Nerd Font" ];
-        emoji = [ "Noto Color Emoji" ];
       };
     };
     fonts = with pkgs; [
       emacs-all-the-icons-fonts
-      fira-code
       fira-code-symbols
       font-awesome
-      liberation_ttf
       nerdfonts
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
     ];
   };
 }
