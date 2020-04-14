64c8e7cb8cb2efb03015fd423cc1a54923569370
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-13 18:36:02 -0600

    format kitty

--- .config/nixos/terminal/kitty.nix
+++ .config/nixos/terminal/kitty.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }: {
-  environment.shellAliases = {
-    icat = "kitty +kitten icat";
-  };
+  environment.shellAliases = { icat = "kitty +kitten icat"; };
   home-manager.users.eduardo = { pkgs, ... }: {
     programs.kitty = {
       enable = true;
@@ -15,29 +13,29 @@
         foreground = "#f8f8f2";
         selection_foreground = "#44475a";
         selection_background = "#f8f8f2";
-        url_color  = "#ffb86c";
-        color0   = "#21222c";
-        color8   = "#6272a4";
-        color1   = "#ff5555";
-        color9   = "#ff6e6e";
-        color2   = "#50fa7b";
-        color10  = "#69ff94";
-        color3   = "#f1fa8c";
-        color11  = "#ffffa5";
-        color4   = "#bd93f9";
-        color12  = "#d6acff";
-        color5   = "#ff79c6";
-        color13  = "#ff92df";
-        color6   = "#8be9fd";
-        color14  = "#a4ffff";
-        color7   = "#f8f8f2";
-        color15  = "#ffffff";
-        cursor  = "#f8f8f2";
+        url_color = "#ffb86c";
+        color0 = "#21222c";
+        color8 = "#6272a4";
+        color1 = "#ff5555";
+        color9 = "#ff6e6e";
+        color2 = "#50fa7b";
+        color10 = "#69ff94";
+        color3 = "#f1fa8c";
+        color11 = "#ffffa5";
+        color4 = "#bd93f9";
+        color12 = "#d6acff";
+        color5 = "#ff79c6";
+        color13 = "#ff92df";
+        color6 = "#8be9fd";
+        color14 = "#a4ffff";
+        color7 = "#f8f8f2";
+        color15 = "#ffffff";
+        cursor = "#f8f8f2";
         cursor_text_color = "background";
-        active_tab_foreground    = "#44475a";
-        active_tab_background    = "#f8f8f2";
-        inactive_tab_foreground  = "#282a36";
-        inactive_tab_background  = "#6272a4";
+        active_tab_foreground = "#44475a";
+        active_tab_background = "#f8f8f2";
+        inactive_tab_foreground = "#282a36";
+        inactive_tab_background = "#6272a4";
         background_opacity = "0.75";
         dim_opacity = "0.75";
 
