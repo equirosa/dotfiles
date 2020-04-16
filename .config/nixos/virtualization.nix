8f6ec60b47d6ef23d1ed234b2463d0963a0156af
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-15 22:54:11 -0600

    make qemu not run as root because i have common sense

--- .config/nixos/virtualization.nix
+++ .config/nixos/virtualization.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }: {
   boot.extraModprobeConfig = "options kvm_intel nested=1";
-  virtualisation = { libvirtd = { enable = true; }; };
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuRunAsRoot = false;
+    };
+  };
   users.users.eduardo = {
     extraGroups = [ "libvirtd" ];
     packages = with pkgs; [ gnome3.gnome-boxes ];
