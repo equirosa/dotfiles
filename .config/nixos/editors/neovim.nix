cb55807dc9e1fe65b1d47bc84e7b3d66a921f18d
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-04-23 17:15:27 -0600

    make neovim set filetype to scheme for guile files

--- .config/nixos/editors/neovim.nix
+++ .config/nixos/editors/neovim.nix
@@ -28,6 +28,7 @@
         let g:lf_replace_netrw = 1 "open lf when vim open a directory
         set colorcolumn=80
         autocmd BufRead,BufNewFile *.ms,*.me,*.mom,*.man set filetype=groff
+        autocmd BufRead,BufNewFile *.guile set filetype=scheme
         autocmd BufWritePost *sway/config,*waybar/* !swaymsg reload
         autocmd BufWritePre,FileWritePre *.nix %!nixfmt
         map <leader>C :w! \| !compiler <c-r>%<CR>
