914444c1868e01ef8b74d76934a3fbe3ecf13f3e
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-05-13 23:54:32 -0600

    remove next browswr from packages
    i'm not really using it, awesome as it is

--- .config/nixos/global.nix
+++ .config/nixos/global.nix
@@ -61,7 +61,6 @@
       #### Browsers ####
       firefox # -wayland
       lynx # Lightweight cli web browser
-      next # The other best browser ever
       qutebrowser # Best browser ever
       tor-browser-bundle-bin # Tor Browser
 
