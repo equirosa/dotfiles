ee3217b8fe8da563b29f7343d44d90443611fe2b
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-05-20 14:38:07 -0600

    add autoscrubbing for btrfs in snowfort

--- .config/nixos/computers/snowfort.nix
+++ .config/nixos/computers/snowfort.nix
@@ -1,4 +1,10 @@
 { config, pkgs, ... }: {
   imports = [ ../global.nix ];
   networking = { hostName = "snowfort"; };
+  services = {
+    btrfs.autoScrub = {
+      enable = true;
+      fileSystems = [ "/home" ];
+    };
+  };
 }
