a76fde1901a1b4a77d7195090f7be8c35473b998
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-07-14 14:04:28 -0600

    nixos: add mpd

--- .config/nixos/audio.nix
+++ .config/nixos/audio.nix
@@ -1,4 +1,14 @@
 { config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ pulsemixer ];
-  hardware.pulseaudio = { enable = true; };
+  hardware.pulseaudio = { enable = true; extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1"; };
+  services.mpd = {
+    enable = true;
+    extraConfig = ''
+      audio_output {
+        type "pulse"
+        name "Pulseaudio"
+        server "127.0.0.1"
+      }
+    '';
+  };
 }
