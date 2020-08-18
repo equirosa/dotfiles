6c657860c9bb0452b632ce977ddcb882e5f0dc30
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-08-18 16:18:48 -0600

    cleanup emacs override

--- .config/nixos/overrides/emacs.nix
+++ .config/nixos/overrides/emacs.nix
@@ -1,46 +1,47 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 let
-  myEmacs = (pkgs.emacs.override {
-    withGTK3 = true;
-    withGTK2 = false;
-  }).overrideAttrs (attrs: {
-    postInstall = (attrs.postInstall or "") + ''
-      rm $out/share/applications/emacs.desktop
-    '';
-  });
+  myEmacs = (
+    pkgs.emacs.override {
+      withGTK3 = true;
+      withGTK2 = false;
+    }
+  ).overrideAttrs (
+    attrs: {
+      postInstall = (attrs.postInstall or "") + ''
+        rm $out/share/applications/emacs.desktop
+      '';
+    }
+  );
   emacsWithPackages = (pkgs.emacsPackagesGen myEmacs).emacsWithPackages;
-in emacsWithPackages (epkgs:
-  (with epkgs.melpaStablePackages; [ ]) ++ (with epkgs.melpaPackages; [
-    # Completions
-    eglot
-    bash-completion
-    # Cosas de GO
-    go-mode
-    company-go
-    # Cosas de RUST
-    cargo
-    rust-mode
-    racer
-    flycheck-rust
-    # Cosas de Nix
-    nix-mode
-    # Cosas de Markdown
-    auto-org-md
-    markdown-mode
-    # Misc.
-    pandoc-mode
-    all-the-icons
-    password-store
-    company
-    keychain-environment
-    evil
-    magit
-    transmission
-    scribble-mode
-    geiser
-    #### Theming ####
-    gruvbox-theme
-    #### Org ####
-    babel
-  ]) ++ (with epkgs.elpaPackages; [ auctex beacon ])
-  ++ (with pkgs; [ emacs-all-the-icons-fonts ]))
+in
+emacsWithPackages (
+  epkgs:
+    (with epkgs.melpaStablePackages; []) ++ (
+      with epkgs.melpaPackages; [
+        # Completions
+        eglot
+        bash-completion
+        # Cosas de Nix
+        nix-mode
+        # Cosas de Markdown
+        auto-org-md
+        markdown-mode
+        # Misc.
+        pandoc-mode
+        all-the-icons
+        password-store
+        company
+        keychain-environment
+        evil
+        magit
+        scribble-mode
+        skribe
+        geiser
+        #### Theming ####
+        gruvbox-theme
+        #### Org ####
+        babel
+      ]
+    ) ++ (with epkgs.elpaPackages; [ auctex beacon ])
+    ++ (with pkgs; [ emacs-all-the-icons-fonts ])
+)
