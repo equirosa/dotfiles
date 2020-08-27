9ec8ea6f8e4904cb00d12a6c8becdca21ae6a2f1
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-08-26 22:46:56 -0600

    fish: autolaunch tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 # Interactive stuff
 if status --is-interactive
+	if set -q $TMUX
+		exec tmux
+	end
 	any-nix-shell fish --info-right | source
 	keychain --eval --quiet -Q sourcehut gitlab github codeberg B77F36C3F12720B4 | source
 	starship init fish | source
