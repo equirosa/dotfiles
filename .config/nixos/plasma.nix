5b622e2f9a5fd1e83139ce280860a896ff8876b8
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-09-04 14:31:59 -0600

    nixos: remove plasma config

--- .config/nixos/plasma.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  imports = [ ./xserver.nix ];
-  environment = { systemPackages = with pkgs; [ ark ]; };
-  nixpkgs.config = { firefox.enablePlasmaBrowserIntegration = true; };
-  services.xserver = {
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
-}
