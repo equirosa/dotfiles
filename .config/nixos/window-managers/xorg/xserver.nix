6e38b92a97e5d368c226fa398aa029579d332a42
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-09-07 23:52:28 -0600

    xserver: remove patches to st and slock

--- .config/nixos/window-managers/xorg/xserver.nix
+++ .config/nixos/window-managers/xorg/xserver.nix
@@ -3,36 +3,8 @@
   programs = {
     slock.enable = true;
   };
-  nixpkgs = {
-    config = {
-      slock = pkgs.stdenv.lib.overrideDerivation pkgs.slock (
-        attrs: rec {
-          patches = map pkgs.fetchurl [
-            {
-              url = "https://tools.suckless.org/slock/patches/blur-pixelated-screen/slock-blur_pixelated_screen-1.4.diff";
-              sha256 = "0bp4p4rgcx1nbw5a69p0x2j014nsfyxaafzih5ky27kki81ls987";
-            }
-          ];
-        }
-      );
-      st = pkgs.stdenv.lib.overrideDerivation pkgs.st (
-        attrs: rec {
-          patches = map pkgs.fetchurl [
-            #{
-            #url = "https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff";
-            #sha256 = "11dj1z4llqbbki5cz1k1crr7ypnfqsfp7hsyr9wdx06y4d7lnnww";
-            #}
-            {
-              url = "https://st.suckless.org/patches/gruvbox/st-gruvbox-dark-0.8.2.diff";
-              sha256 = "0bhvw1jam9s0km5hwbnicb27sgwzj04msmwc8gvpf2islpnxbcsf";
-            }
-          ];
-        }
-      );
-    };
-  };
   environment = {
-    sessionVariables = { LOCK_CMD = "slock"; };
+    #sessionVariables = { LOCK_CMD = "slock"; };
     systemPackages = with pkgs; [
       dmenu
       maim
