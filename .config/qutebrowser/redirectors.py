b85f780906be1d761c2b0f933ba2921d58b0058a
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-09-10 01:27:39 -0600

    qutebrowser: remove unused redirections

--- .config/qutebrowser/redirectors.py
+++ .config/qutebrowser/redirectors.py
@@ -36,7 +36,7 @@ def _hastebin_redir(url: QUrl) -> bool:
 	return False
 
 def _invidious_instance() -> str:
-    instances = [ 'invidio.us', 'invidious.snopyta.org', 'invidious.13ad.de', 'invidious.fdn.fr', 'watch.nettohikari.com' ]
+    instances = [ 'invidio.us', 'invidious.snopyta.org' ]
     return random.choice(instances)
 
 def _nitter_instance() -> str:
