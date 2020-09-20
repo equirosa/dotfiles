ff8002e0cc75f4998220bc52f9fbb46e71c27200
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-09-20 04:46:28 -0600

    emacs: remove unused stuff

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,16 +1,1 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(require 'iso-transl) ;; allow some config
-(load-theme 'gruvbox-dark-hard t) ;; load preferred theme
 (global-visual-line-mode t) ;; add visual line wrapping
