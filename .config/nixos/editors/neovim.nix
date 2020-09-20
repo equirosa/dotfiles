f402dcebb496b980d081e51148795d1ffe3ed37f
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-09-19 23:34:33 -0600

    nvim: nixfmt config

--- .config/nixos/editors/neovim.nix
+++ .config/nixos/editors/neovim.nix
@@ -6,32 +6,33 @@
       vim = "nvim";
       vimdiff = "nvim -d";
     };
-    systemPackages = with pkgs; [
-      (neovim.override {
-        viAlias = true;
-        vimAlias = true;
-        configure = {
-          packages.myPlugins = with pkgs.vimPlugins; {
-            start = [
-        auto-pairs
-        bclose-vim
-        fugitive
-        gruvbox-community
-        lf-vim
-        nerdcommenter
-        lightline-vim
-        vim-automkdir
-        vim-devicons
-        vim-surround
-        YouCompleteMe
-        # Syntax highlighting
-        vim-css-color
-        vim-polyglot
-            ];
-            opt = [];
+    systemPackages = with pkgs;
+      [
+        (neovim.override {
+          viAlias = true;
+          vimAlias = true;
+          configure = {
+            packages.myPlugins = with pkgs.vimPlugins; {
+              start = [
+                auto-pairs
+                bclose-vim
+                fugitive
+                gruvbox-community
+                lf-vim
+                nerdcommenter
+                lightline-vim
+                vim-automkdir
+                vim-devicons
+                vim-surround
+                YouCompleteMe
+                # Syntax highlighting
+                vim-css-color
+                vim-polyglot
+              ];
+              opt = [ ];
+            };
           };
-        };
-      })
-    ];
+        })
+      ];
   };
 }
