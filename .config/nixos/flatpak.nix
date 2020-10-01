664884444673ebc26b278e0201d545e8cfb8b807
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-10-01 00:42:18 -0600

    flatpak: add some portals

--- .config/nixos/flatpak.nix
+++ .config/nixos/flatpak.nix
@@ -8,7 +8,11 @@
   services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-kde ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-kde
+      xdg-desktop-portal-wlr
+    ];
     gtkUsePortal = true;
   };
 }
