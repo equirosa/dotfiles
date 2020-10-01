ab766603e4b1c2674f58155dc198573aca9a62eb
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-10-01 00:08:37 -0600

    neovim: nixfmt

--- .config/nixos/editors/neovim.nix
+++ .config/nixos/editors/neovim.nix
@@ -8,36 +8,40 @@
     };
     systemPackages = with pkgs;
       [
-        (neovim.override {
-          viAlias = true;
-          vimAlias = true;
-          configure = {
-            packages.myPlugins = with pkgs.vimPlugins; {
-              start = [
-                auto-pairs
-                bclose-vim
-                fugitive
-                gruvbox-community
-                lf-vim
-                nerdcommenter
-                lightline-vim
-                vim-automkdir
-                vim-devicons
-                vim-surround
-                # CoC
-                coc-nvim
-                fzf-vim
-                coc-fzf
-                # Syntax highlighting
-                rainbow
-                vim-css-color
-                vim-polyglot
-              ];
-              opt = [ ];
+        (
+          neovim.override {
+            viAlias = true;
+            vimAlias = true;
+            configure = {
+              packages.myPlugins = with pkgs.vimPlugins; {
+                start = [
+                  auto-pairs
+                  bclose-vim
+                  fugitive
+                  gruvbox-community
+                  lf-vim
+                  nerdcommenter
+                  lightline-vim
+                  vim-automkdir
+                  vim-devicons
+                  vim-surround
+                  # CoC
+                  coc-nvim # Main one
+                  fzf-vim # Fzf integration for Vim
+                  coc-fzf # Fzf integration for CoC
+                  coc-vimlsp # VimScript integration for CoC
+                  coc-prettier # formatting
+                  # Syntax highlighting
+                  rainbow
+                  vim-css-color
+                  vim-polyglot
+                ];
+                opt = [];
+              };
+              customRC = "so /home/eduardo/.config/nvim/init.vim";
             };
-            customRC = "so /home/eduardo/.config/nvim/init.vim";
-          };
-        })
+          }
+        )
       ];
   };
 }
