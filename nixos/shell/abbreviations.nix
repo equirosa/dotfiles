f5681412a58f32659af864604b716ad8afeadf79
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-13 18:49:04 -0600

    add new abbrs

--- nixos/shell/abbreviations.nix
+++ nixos/shell/abbreviations.nix
@@ -2,7 +2,11 @@
   cl = "curl -L";
   clo = "curl -LO";
   l = "ls -l";
+  lg = "lazygit";
   ll = "ls -la";
+  md = "mkdir -p";
   ns = "nix search nixpkgs";
   nsp = "nix-shell -p";
+  v = "mpv";
+  yf = "ytfzf";
 }
