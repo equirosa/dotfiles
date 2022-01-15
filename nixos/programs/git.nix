54585b15f65cd155817b87278daabbfb00e02316
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-15 03:32:10 -0600

    nixos: install github cli

--- nixos/programs/git.nix
+++ nixos/programs/git.nix
@@ -40,6 +40,17 @@
         enable = true;
         settings = { };
       };
+      gh = {
+        enable = true;
+        settings = {
+          git_protocol = "ssh";
+          prompt = "enabled";
+          aliases = {
+            co = "pr checkout";
+            pv = "pr view";
+          };
+        };
+      };
     };
   };
 }
