bda172302962156d5c3d91a56cd2f6d1d26ba9e8
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-19 17:40:45 -0600

    abbrs: fix references

--- nixos/shell/abbreviations.nix
+++ nixos/shell/abbreviations.nix
@@ -1,17 +1,19 @@
 {
   cl = "curl -L";
   clo = "curl -LO";
+  cnf = "command-not-found";
   gp = "gopass";
   gpn = "gopass new";
   l = "ls -l";
   lg = "lazygit";
   ll = "ls -la";
+  m = "mpv";
   md = "mkdir -p";
   nj = "nixpkgs-info-json";
   nr = "nix run nixpkgs#";
   ns = "nix search nixpkgs";
   nsp = "nix-shell -p";
   tl = "tldr";
-  v = "mpv";
+  v = "$EDITOR";
   yf = "ytfzf";
 }
