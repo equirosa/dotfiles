ee13406bdb81f5fa8966805632d29c71c78282c7
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-22 00:05:53 -0600

    enable direnv for nix

--- nixos/global.nix
+++ nixos/global.nix
@@ -25,6 +25,10 @@ in
         };
       };
       programs = {
+        direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
         gpg = {
           enable = true;
         };
