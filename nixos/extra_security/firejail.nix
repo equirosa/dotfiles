c88e9216e296128b0ece73e060eeb93b3fb26795
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-31 18:16:37 -0600

    firejail: sandbox librewolf

--- nixos/extra_security/firejail.nix
+++ nixos/extra_security/firejail.nix
@@ -7,6 +7,9 @@
           executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
           profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
           }; */
+        librewolf = {
+          executable = "${pkgs.lib.getBin pkgs.nur.repos.wolfangaukang.librewolf}/bin/librewolf";
+          profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
         };
       };
     };
