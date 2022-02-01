ead3d8292f21b42740de673646a91c98b704735b
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-01-31 18:24:24 -0600

    remove unnecessary closing brace

--- nixos/extra_security/firejail.nix
+++ nixos/extra_security/firejail.nix
@@ -7,7 +7,6 @@
           executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
           profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
           }; */
-        };
       };
     };
   };
