ae54619f909956a4f57d364ad0ae98b860d1e5fa
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-03-26 18:06:10 -0600

    qemu: install virt-manager too

--- nixos/virtualization/qemu.nix
+++ nixos/virtualization/qemu.nix
@@ -1,7 +1,6 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   boot.extraModprobeConfig = "options kvm_amd nested=1";
   virtualisation = {
@@ -15,11 +14,9 @@
   };
   programs.dconf.enable = true;
   users.users.kiri = {
-    extraGroups = ["libvirtd"];
+    extraGroups = [ "libvirtd" ];
     packages = with pkgs; [
-      /*
-       virt-manager
-       */
+      virt-manager
       quickemu
     ];
   };
