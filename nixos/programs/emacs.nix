92b6d98664f994c2797248cc960c773fb2cc72a4
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-04-13 09:40:12 -0600

    emacs: include vterm through nix

--- nixos/programs/emacs.nix
+++ nixos/programs/emacs.nix
@@ -1,55 +1,29 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   nixpkgs.overlays = [
     (
       import
-      (builtins.fetchTarball {url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";})
+        (builtins.fetchTarball { url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"; })
     )
   ];
   home-manager.users.kiri = {
     services.emacs = {
       enable = true;
-      client = {enable = true;};
+      client = { enable = true; };
       socketActivation.enable = false;
     };
     programs.emacs = {
       enable = true;
       package = pkgs.emacsPgtkGcc;
-      /*
-        extraPackages = epkgs:
-       with epkgs; [
-         # Programming
-         company
-         tree-sitter
-         tree-sitter-langs
-         # Miscellaneous
-         linum-relative
-         undo-tree
-         # Vi keybinds
-         evil
-         # Nix
-         envrc
-         nix-mode
-         nixpkgs-fmt
-         # Org
-         org-modern
-         org-super-agenda
-         org-timeline
-         org-roam
-         org-roam-ui
-         org-roam-bibtex
-         # Style
-         all-the-icons
-         all-the-icons-dired
-         doom-modeline
-         gruvbox-theme
-         rainbow-delimiters
-       ];
-       */
+
+      extraPackages = epkgs:
+        with epkgs; [
+          # Utilities
+          vterm
+        ];
     };
-    home.packages = with pkgs; [sqlite gcc];
+    home.packages = with pkgs; [ sqlite gcc ];
   };
 }
