8a23a883cf270149e4c1033ab23a73501a10e346
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-04-13 01:48:11 -0600

    fish: format nix config

--- nixos/shell/fish.nix
+++ nixos/shell/fish.nix
@@ -1,28 +1,28 @@
-{pkgs, ...}: let
+{ pkgs, config, lib, ... }:
+let
   abbreviations = import ./abbreviations.nix;
   aliases = import ./aliases.nix;
-in {
+in
+{
   users.users.kiri.shell = pkgs.fish;
   home-manager.users.kiri = {
     programs = {
       fish = {
         enable = true;
+        # shellAliases = aliases;
         shellAbbrs = abbreviations;
-        #shellAliases = aliases;
         interactiveShellInit = ''
           ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
         '';
         plugins = [
           {
             name = "done";
-            src =
-              pkgs.fetchFromGitHub
-              {
-                owner = "franciscolourenco";
-                repo = "done";
-                rev = "1.16.5";
-                sha256 = "sha256-E0wveeDw1VzEH2kzn63q9hy1xkccfxQHBV2gVpu2IdQ=";
-              };
+            src = pkgs.fetchFromGitHub {
+              owner = "franciscolourenco";
+              repo = "done";
+              rev = "1.16.5";
+              sha256 = "sha256-E0wveeDw1VzEH2kzn63q9hy1xkccfxQHBV2gVpu2IdQ=";
+            };
           }
         ];
       };
