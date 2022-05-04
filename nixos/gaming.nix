c6dc24fffb04a7a1fd9a505a964765d5ac541bbd
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-05-04 12:58:21 -0600

    uninstall gamescope temporarily

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -26,7 +26,7 @@ in {
         taisei
         warzone2100
         zeroad
-        nur.repos.dukzcry.gamescope
+        # nur.repos.dukzcry.gamescope
         nix-gaming.packages.x86_64-linux.rocket-league
         # Launchers
         lutris
