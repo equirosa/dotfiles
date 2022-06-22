46714b2c73a495cb452b8c23249323e3e1b7690a
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-06-22 17:08:52 -0600

    enable nix unstable

--- nixos/global.nix
+++ nixos/global.nix
@@ -78,6 +78,7 @@ in {
       persistent = true;
     };
     settings = {auto-optimise-store = true;};
+    package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = flakes nix-command
       keep-outputs = true
