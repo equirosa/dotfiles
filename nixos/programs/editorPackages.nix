cc257ad6c1be0c3bc391eeca939f073738d377aa
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-09 02:24:26 -0600

    editor-only-packages: install yaml-language-server

--- nixos/programs/editorPackages.nix
+++ nixos/programs/editorPackages.nix
@@ -4,6 +4,7 @@ with pkgs; [
   nodePackages.prettier
   nodePackages.typescript
   nodePackages.typescript-language-server
+  nodePackages.yaml-language-server
   rnix-lsp
   rust-analyzer
   sumneko-lua-language-server
