290e8b64fa707c0533ed86caefe1fb50c5aa99b4
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-22 01:20:01 -0600

    add initial configuration in home-manager for i3status-rust

---
+++ nixos/graphical/wayland/bar/i3status-rust.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  home-manager.users.kiri.programs.i3status-rust = {
+    enable = true;
+    bars.top = {
+      blocks = [
+        {
+          block = "disk_space";
+          path = "/";
+          unit = "GB";
+          interval = 60;
+          warning = 30.0;
+          alert = 10.0;
+        }
+        {
+          block = "keyboard_layout";
+          driver = "sway";
+        }
+      ];
+    };
+  };
+}
