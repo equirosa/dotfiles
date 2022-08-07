b860b87dd4bba1ad579a8344dfe11b9ba6ab2e54
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-07 02:42:13 -0600

    add abbreviation to set nvim as EDITOR

--- nixos/shell/abbreviations.nix
+++ nixos/shell/abbreviations.nix
@@ -26,6 +26,7 @@ in {
   ns = "nix search nixpkgs";
   nsp = "nix-shell -p";
   nup = "nix-update --test --commit --review";
+  sen = "export EDITOR=nvim";
   sid = "setsid";
   tl = "tldr";
   u = "umpv";
