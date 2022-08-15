8ada27d91be95fccd868c72b4430b225fae8ec1b
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-15 02:53:26 -0600

    fix statix check errors

--- nixos/programs/neovim.nix
+++ nixos/programs/neovim.nix
@@ -4,7 +4,7 @@
 }: {
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+      url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
     }))
   ];
   home-manager.users.kiri = {
