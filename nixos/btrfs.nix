5fd08d2576e278c0ad9bcdd2efd272bf0e2fcd8a
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-16 22:13:40 -0600

    run bees in a more limited capacity

--- nixos/btrfs.nix
+++ nixos/btrfs.nix
@@ -6,6 +6,9 @@
     beesd.filesystems = {
       root = {
         spec = "/";
+        hashTableSizeMB = 1024;
+        verbosity = "crit";
+        extraOptions = [ "--loadavg-target" "3.5" ];
       };
     };
     btrfs = {
