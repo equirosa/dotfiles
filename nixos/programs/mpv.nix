bd40b4eb1d19049a3445c0856b09660f5206f2a6
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-09-28 00:35:13 -0600

    mpv: finally use the home-manager module correctly

--- nixos/programs/mpv.nix
+++ nixos/programs/mpv.nix
@@ -1,30 +1,23 @@
 {pkgs, ...}: {
   home-manager.users.kiri = {config, ...}: {
-    home.packages = [
-      (pkgs.wrapMpv
-        (pkgs.mpv-unwrapped)
-        {
-          scripts = with pkgs.mpvScripts; [
-            mpv-playlistmanager
-            sponsorblock
-          ];
-        })
-    ];
-    xdg.configFile = {
-      "mpv.conf" = {
-        target = "mpv/mpv.conf";
-        text = ''
-          force-window=yes
-          save-position-on-quit
-          screenshot-directory=${config.xdg.userDirs.pictures}/screenshots
-          gpu-context=wayland
-          hwdec=auto-safe
-          vo=gpu
-          profile=gpu-hq
-          slang=en,eng
-          ytdl-format='(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best'
-        '';
+    programs.mpv = {
+      enable = true;
+      defaultProfiles = ["gpu-hq"];
+      config = {
+        force-window = true;
+        save-position-on-quit = "";
+        screenshot-directory = "${config.xdg.userDirs.pictures}/screenshots";
+        gpu-context = "wayland";
+        hwdec = "auto-safe";
+        vo = "gpu";
+        slang = "en,eng";
+        alang = "en,eng";
+        ytdl-format = "(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best";
       };
+      scripts = with pkgs.mpvScripts; [
+        mpv-playlistmanager
+        sponsorblock
+      ];
     };
   };
 }
