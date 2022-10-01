9486138979207e9918b5ce30d68cb0be9767ce01
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-09-30 18:31:34 -0600

    add tofi config file

--- nixos/programs/default.nix
+++ nixos/programs/default.nix
@@ -26,6 +26,18 @@ in {
     ./newsboat.nix
   ];
   home-manager.users.kiri = {config, ...}: {
+    xdg.configFile."tofi/config".text = ''
+      width = 100%
+      height = 100%
+      border-width = 0
+      outline-width = 0
+      padding-left = 35%
+      padding-top = 35%
+      result-spacing = 25
+      num-results = 5
+      font = monospace
+      background-color = #000A
+    '';
     home.packages = with pkgs;
       [
         # Browsers
