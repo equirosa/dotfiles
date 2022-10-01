00ce67dd6dfd79d9f6b86849b3ff67d4e7681bc7
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-09-30 18:28:26 -0600

    mpv: add profiles

--- nixos/programs/mpv.nix
+++ nixos/programs/mpv.nix
@@ -15,6 +15,14 @@
         alang = "en,eng";
         ytdl-format = "(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best";
       };
+      profiles = {
+        "protocol.https" = {
+          profile-desc = "Profile for web videos";
+          speed = 3.14;
+          keep-open = "";
+        };
+        "protocol.http".profile = "protocol.https";
+      };
       scripts = with pkgs.mpvScripts; [
         mpv-playlistmanager
         sponsorblock
