2d8223e74132e962847c1e425739c9a001eacc1c
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-11-16 19:11:03 -0600

    use adwaita theming

--- nixos/theme.nix
+++ nixos/theme.nix
@@ -24,26 +24,27 @@
       twitter-color-emoji
     ];
   };
-  home-manager.users.kiri = { pkgs, ... }: {
-    gtk = {
-      enable = true;
-      iconTheme = {
-        package = pkgs.papirus-icon-theme;
-        name = "Papirus-Dark";
+  home-manager.users.kiri = { pkgs, ... }:
+    let
+      gtk-extra-config = { gtk-application-prefer-dark-theme = true; };
+    in
+    {
+      gtk = {
+        enable = true;
+        iconTheme = {
+          package = pkgs.gnome.adwaita-icon-theme;
+          name = "Adwaita";
+        };
+        gtk3.extraConfig = gtk-extra-config;
+        gtk4.extraConfig = gtk-extra-config;
       };
-      theme = {
-        package = pkgs.materia-theme;
-        name = "Materia-dark-compact";
+      qt = {
+        enable = true;
+        platformTheme = "gnome";
+        style = {
+          package = pkgs.adwaita-qt;
+          name = "adwaita-dark";
+        };
       };
-      gtk3.extraConfig = { gtk-application-prefer-dark-theme = true; };
     };
-    qt = {
-      enable = true;
-      platformTheme = "gnome";
-      style = {
-        package = pkgs.materia-kde-theme;
-        name = "Adwaita-Dark";
-      };
-    };
-  };
 }
