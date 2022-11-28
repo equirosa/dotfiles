5398bd35fa8f48a9e124a25b21b2e1bca1d800de
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-11-27 22:46:57 -0600

    gnupg: switch pinentry flavor to gnome3's

--- nixos/global.nix
+++ nixos/global.nix
@@ -83,6 +83,7 @@ in
           maxCacheTtl = 34560000;
           enableScDaemon = false;
           enableSshSupport = true;
+          pinentryFlavor = "gnome3";
           extraConfig = ''
             allow-emacs-pinentry
             allow-loopback-pinentry
@@ -136,6 +137,7 @@ in
   };
   security = { pam = { services = { login = { gnupg.enable = true; }; }; }; };
   services = {
+    dbus.packages = [ pkgs.gcr ];
     dnscrypt-proxy2 = {
       enable = true;
       settings = {
