00d420ff378080038caf68ad52bbbc1e49ff421a
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-12-10 20:44:15 -0600

    helix: fix config

--- nixos/programs/editors/helix.nix
+++ nixos/programs/editors/helix.nix
@@ -3,16 +3,29 @@
     enable = true;
     languages = [
       { name = "elm"; auto-format = true; }
+      { name = "nix"; auto-format = true; }
     ];
     settings = {
-      theme = "base16";
-      lsp = { display-messages = true; };
+      theme = "onedark";
+      editor = {
+        line-number = "relative";
+        mouse = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };
       keys = {
         normal = {
           space = {
             space = "file_picker";
             w = ":w";
             q = ":q";
+          };
+          Z = {
+            Q = ":q!";
+            Z = ":wq";
           };
         };
       };
