9ecebb3f27e6cb2b53df336172f6eca35521cf91
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-01-26 22:22:59 -0600

    btrfs: enable btrbk for home directory

--- nixos/btrfs.nix
+++ nixos/btrfs.nix
@@ -9,6 +9,16 @@
       verbosity = "crit";
       extraOptions = [ "--loadavg-target" "2.5" ];
     };
+    btrbk.instances."home" = {
+      onCalendar = "*:0/10";
+      settings = {
+        snapshot_preserve_min = "2d";
+        volume."/" = {
+          subvolume = "/home";
+          snapshot_dir = ".snapshots";
+        };
+      };
+    };
     btrfs.autoScrub = {
       enable = true;
       fileSystems = [ "/" ];
