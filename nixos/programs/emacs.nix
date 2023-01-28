68f7fa01dc8b27277ea7af5fcfcc3706471ac873
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-01-28 14:05:12 -0600

    emacs: use normal emacs package
    The normal emacs package uses the exact same version as the one in
    unstable, so there's no real benefit to using the unstable package

--- nixos/programs/emacs.nix
+++ nixos/programs/emacs.nix
@@ -15,7 +15,7 @@
     enable = true;
     package = pkgs.emacsWithPackagesFromUsePackage {
       config = ../../emacs/init.el;
-      package = pkgs.emacsNativeComp;
+      package = pkgs.emacs;
       alwaysEnsure = true;
       alwaysTangle = false;
       defaultInitFile = true;
