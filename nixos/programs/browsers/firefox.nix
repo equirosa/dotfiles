46797e3661f13a610c94ab5082122132620ceff5
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-02-03 08:18:02 -0600

    enable librewolf
    potential main browser replacement

--- nixos/programs/browsers/firefox.nix
+++ nixos/programs/browsers/firefox.nix
@@ -1,9 +1,17 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+let
+  globalSettings = { };
+in
+{
   programs.firefox = {
     enable = true;
     package = pkgs.firefox.override {
       cfg = { enableTridactylNative = true; };
     };
     enableGnomeExtensions = false;
+  };
+  programs.librewolf = {
+    enable = true;
+    settings = { };
   };
 }
