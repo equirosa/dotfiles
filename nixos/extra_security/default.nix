a89c03b3cb0eda3f7cefc48f74c9283d0738bb22
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-02-25 18:43:13 -0600

    clean up security config

--- nixos/extra_security/default.nix
+++ nixos/extra_security/default.nix
@@ -10,22 +10,19 @@
   environment = {
     # Remove default packages to reduce attack surface.
     defaultPackages = lib.mkForce [ ];
-    # memoryAllocator.provider = "graphene-hardened"; # TODO: consider using this one, after a successful boot with scudo.
   };
-  networking = {
-    firewall =
-      let
-        openPortRanges = [ ];
-        open-ports = [ ];
-      in
-      {
-        # Close firewall
-        enable = true;
-        allowedTCPPorts = open-ports;
-        allowedTCPPortRanges = openPortRanges;
-        allowedUDPPorts = open-ports;
-        allowedUDPPortRanges = openPortRanges;
-      };
-  };
-  nix.settings.allowed-users = [ "@wheel" ];
+  networking.firewall =
+    let
+      openPortRanges = [ ];
+      open-ports = [ ];
+    in
+    {
+      # Close firewall
+      enable = true;
+      allowedTCPPorts = open-ports;
+      allowedTCPPortRanges = openPortRanges;
+      allowedUDPPorts = open-ports;
+      allowedUDPPortRanges = openPortRanges;
+    };
+    nix.settings.allowed-users = [ "@wheel" ];
 }
