3dace2037b554fe5b50f28deb5bf2415ea234e3f
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-04 23:38:26 -0600

    wayland: segregate notification config

--- nixos/graphical/wayland/sway.nix
+++ nixos/graphical/wayland/sway.nix
@@ -21,7 +21,7 @@ let
   colors = import ../../colors.nix;
 in
 {
-  imports = [ ./bar ];
+  imports = [ ./bar ./notifications.nix ];
   home-manager.users.kiri =
     { config
     , lib
@@ -47,35 +47,6 @@ in
         ];
       };
       programs = {
-        mako = {
-          enable = true;
-          backgroundColor = "#${colors.selected.background}";
-          borderColor = "#${colors.selected.bright.yellow}";
-          borderRadius = 15;
-          borderSize = 2;
-          defaultTimeout = 5000;
-          font = "monospace 14";
-          height = 110;
-          layer = "top"; # Consider overlay
-          markup = true;
-          sort = "-time";
-          extraConfig = with colors.selected.bright; ''
-            [urgency=low]
-            border-color=#${green}
-
-            [urgency=normal]
-            border-color=#${yellow}
-
-            [urgency=high]
-            border-color=#${red}
-            default-timeout=0
-
-            [category=mpd]
-            border-color=#${blue}
-            default-timeout=2000
-            group-by=category
-          '';
-        };
         # Foot config. TODO: consider moving to own file
         foot = {
           enable = true;
