2ae5e5a651d1417124301d374ad9973e596b2f96
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-06 00:44:30 -0600

    enable vscode

---
+++ nixos/programs/editors/vscode.nix
@@ -0,0 +1,16 @@
+{ pkgs, config, ... }: {
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions;[
+      mkhl.direnv
+      pkief.material-icon-theme
+    ];
+    mutableExtensionsDir = false;
+    userSettings = {
+      "files.autoSave" = "off";
+      "[nix]"."editor.tabSize" = 2;
+    };
+  };
+}
