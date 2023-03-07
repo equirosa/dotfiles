6f90c3c76a46d21705e0ba03da93a39b74378480
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-07 09:09:14 -0600

    vscode: install nix stuff

--- nixos/programs/editors/vscode.nix
+++ nixos/programs/editors/vscode.nix
@@ -4,6 +4,9 @@
     enableUpdateCheck = false;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions;[
+      arrterian.nix-env-selector
+      bbenoist.nix
+      jnoortheen.nix-ide
       mkhl.direnv
       pkief.material-icon-theme
     ];
