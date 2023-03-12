d501aa59b831f6870bcb18f71d27bc0c17f33e8a
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-12 16:44:02 -0600

    i3status-rust: remove extra "%" from format

--- nixos/graphical/wayland/bar/i3status-rust.nix
+++ nixos/graphical/wayland/bar/i3status-rust.nix
@@ -37,7 +37,7 @@
         }
         {
           block = "sound";
-          format = "{output_name} {volume}%";
+          format = "{output_name} {volume}";
           # on_click = "pavucontrol --tab=3";
           mappings = {
             "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
