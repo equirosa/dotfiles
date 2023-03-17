726c19ea4ea56994ca9ced297b43d94efc0d9ab2
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-16 22:19:47 -0600

    format

--- nixos/programs/editors/emacs-config/init.el
+++ nixos/programs/editors/emacs-config/init.el
@@ -36,13 +36,14 @@
   :bind ("C-c b" . bool-flip-do-flip))
 (use-package hl-todo
   :custom
-  (hl-todo-keyword-faces '(("TODO"   . "#FF0000")
-			   ("FIXME"  . "#FF0000")
-			   ("DEBUG"  . "#A020F0")
-			   ("GOTCHA" . "#FF4500")
-			   ("HACK"   . "#D79921")
-			   ("STUB"   . "#1E90FF")
-			   ("DONE"   . "#00FF00")))
+  (hl-todo-keyword-faces
+	 '(("TODO"   . "#FF0000")
+		 ("FIXME"  . "#FF0000")
+		 ("DEBUG"  . "#A020F0")
+		 ("GOTCHA" . "#FF4500")
+		 ("HACK"   . "#D79921")
+		 ("STUB"   . "#1E90FF")
+		 ("DONE"   . "#00FF00")))
   :hook (prog-mode))
 ;; Keybinds (for comfort)
 (defun kiri/kill-this-buffer ()
