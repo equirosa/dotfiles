32d120600991dae750dc2038f63df5b4ca0e56d9
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-04-12 21:53:46 -0600

    emacs: change substituter to a trusted one

--- nixos/programs/editors/emacs.nix
+++ nixos/programs/editors/emacs.nix
@@ -26,7 +26,7 @@ in
     };
   };
   nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
+    trusted-substituters = [ "https://nix-community.cachix.org" ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
