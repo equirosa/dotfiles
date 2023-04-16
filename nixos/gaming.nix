4f0a785608d9123453daec13714dcac85a483100
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-04-16 15:37:29 -0600

    correctly declare the substituters

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -36,8 +36,16 @@ let inherit (builtins) attrValues elem; in
     steam-hardware.enable = true;
   };
   nix.settings = {
-    substituters = [ "https://nix-gaming.cachix.org" ];
-    trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+    nix.settings = {
+      trusted-substituters = [
+        "https://nix-gaming.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
+    };
   };
   nixpkgs = {
     config = {
