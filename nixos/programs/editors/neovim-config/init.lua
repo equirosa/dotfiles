6f856f20439850ae29adfda8954669962f651634
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-10 02:32:33 -0600

    neovim: add keymap to quickly save the buffer

--- nixos/programs/editors/neovim-config/init.lua
+++ nixos/programs/editors/neovim-config/init.lua
@@ -311,6 +311,7 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set({ "n" }, "<leader>gg", ":LazyGit<CR>", { desc = "Run lazygit" })
+vim.keymap.set({ "n" }, "<leader>w", vim.cmd.write, { desc = "Save buffer" })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
