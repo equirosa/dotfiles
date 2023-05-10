bb31dc7faa91b49415706c6e64489b1db0761a0f
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-09 22:23:44 -0600

    emacs: go back to using init.el
    This reverts commit 2af8c1709c26f9124fa5e327d4c0c70c4a5af8cf.

--- nixos/programs/editors/emacs.nix
+++ nixos/programs/editors/emacs.nix
@@ -5,7 +5,7 @@
     enable = true;
     defaultEditor = true;
     package = pkgs.emacsWithPackagesFromUsePackage {
-      config = ./emacs-config/config.el;
+      config = ./emacs-config/init.el;
       package = pkgs.emacsPgtk;
       alwaysEnsure = true;
       alwaysTangle = false;
