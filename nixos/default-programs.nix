db82fdc9b29a3dafcd94ede0d23e812d06e288b1
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-15 14:43:10 -0600

    default-programs: remove unnecessary quoting
    the "getExe" function returns a string anyway, so interpolating it into a string is
    redundant

--- nixos/default-programs.nix
+++ nixos/default-programs.nix
@@ -3,15 +3,15 @@ let
   inherit (lib) getExe;
 in
 {
-  gemini-browser = "${getExe pkgs.lagrange}";
+  gemini-browser = getExe pkgs.lagrange;
   http-browser = "${getExe pkgs.firefox} -p default";
   image-viewer = getExe pkgs.imv;
   notify = "${getExe pkgs.libnotify} -t 5000";
-  terminal = "${getExe pkgs.foot}";
-  terminal-http-browser = "${getExe pkgs.w3m}";
-  terminal-monitor = "${getExe pkgs.btop}";
-  terminal-audio = "${getExe pkgs.pulsemixer}";
-  terminal-file-manager = "${getExe pkgs.lf}";
-  terminal-mail-client = "${getExe pkgs.aerc}";
-  terminal-feed-reader = "${getExe pkgs.newsboat}";
+  terminal = getExe pkgs.foot;
+  terminal-audio = getExe pkgs.pulsemixer;
+  terminal-feed-reader = getExe pkgs.newsboat;
+  terminal-file-manager = getExe pkgs.lf;
+  terminal-http-browser = getExe pkgs.w3m;
+  terminal-mail-client = getExe pkgs.aerc;
+  terminal-monitor = getExe pkgs.btop;
 }
