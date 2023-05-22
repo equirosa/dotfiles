2c136e145edd70a9c3cb91c5a9a165c7eb590199
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-22 17:53:57 -0600

    move hosts config to git repo

---
+++ hosts/snowfort/configuration.nix
@@ -0,0 +1,35 @@
+_: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "snowfort"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/Costa_Rica";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kiri = {
+    isNormalUser = true;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
