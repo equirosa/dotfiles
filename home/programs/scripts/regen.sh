ed0a601df4d2dcd4fa3c9d237cd83905ae8e32cf
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-24 00:30:05 -0600

    regen: add test function

--- home/programs/scripts/regen.sh
+++ home/programs/scripts/regen.sh
@@ -7,8 +7,15 @@ regen_nixos() {
 	sudo nixos-rebuild switch --flake "${flake_path}#$(hostname)"
 }
 
+test_run() {
+	cd "$(mktemp -d)"
+	home-manager build --flake "${flake_path}#main"
+	nixos-rebuild build --flake "${flake_path}#$(hostname)"
+}
+
 case "${1}" in
 	"home") regen_home ;;
 	"os") regen_nixos ;;
 	"all") regen_nixos && regen_home ;;
+	"test") test_run ;;
 esac
