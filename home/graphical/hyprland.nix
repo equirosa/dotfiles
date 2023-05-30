920710414526080d7f40f39dd3ee0aa5281dfc46
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-30 02:01:27 -0600

    hyprland: add more color to window borders

--- home/graphical/hyprland.nix
+++ home/graphical/hyprland.nix
@@ -56,7 +56,7 @@ in {
           gaps_in = ${toString gaps}
           gaps_out = ${toString (gaps * 2)}
           border_size = 2
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+          col.active_border = rgba(E323BEEE) rgba(33ccffee) rgba(00ff99ee) 45deg
           col.inactive_border = rgba(595959aa)
           layout = dwindle
       }
