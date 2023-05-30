4ef58c07b10af5363cb2ec09b70f823dcd1eed9b
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-05-30 14:49:24 -0600

    wezterm: fix theming config

--- home/programs/terminal/wezterm.lua
+++ home/programs/terminal/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font("monospace"),
-  color_scheme = "Gruvbox Dark",
+  color_scheme = "GruvboxDarkHard",
+  window_background_opacity = 0.8,
 }
-
