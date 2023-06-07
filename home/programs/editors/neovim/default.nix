3e009a99063e56b00cea7a13c660ff98cf3531d0
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-06 19:56:28 -0600

    neovim: add nvim-cmp plugin

--- home/programs/editors/neovim/default.nix
+++ home/programs/editors/neovim/default.nix
@@ -43,6 +43,36 @@ _: {
       lsp-format.enable = true;
       lsp-lines.enable = true;
       neogit.enable = true;
+      nvim-cmp = {
+        enable = true;
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            modes = ["i" "s"];
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expandable() then
+                  luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif check_backspace() then
+                  fallback()
+                else
+                  fallback()
+                end
+              end
+            '';
+          };
+        };
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "luasnip";} #For luasnip users.
+          {name = "path";}
+          {name = "buffer";}
+        ];
+      };
       treesitter = {
         enable = true;
         folding = true;
