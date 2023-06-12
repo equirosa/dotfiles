f77877db61e9037f54027c6a2749c6a003fe4076
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-12 01:08:29 -0600

    autostart: add swww daemon

--- home/programs/scripts/autostart.sh
+++ home/programs/scripts/autostart.sh
@@ -4,6 +4,8 @@ if [ "$day" -le 5 ]; then
 	setsid rem-lap
 fi
 
+swww init
+swww img "${XDG_PICTURES_DIR}/desktop_backgrounds/gifs/city.gif"
 setsid transmission-daemon
 setsid element-desktop
 setsid firefox -p default
