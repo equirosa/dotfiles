ff68cc45c594f255e3cd5284dce20e76ee29d0cb
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-25 21:39:44 -0600

    allow vscode unfree predicate

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,8 @@
-_: {
+{lib, ...}: {
   imports = [./graphical ./theme.nix ./shell ./programs];
   programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode"
+    ];
 }
