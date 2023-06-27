63cdf89959fbc1308cfd951cc0dd7a1decbacbff
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-27 17:17:59 -0600

    format

--- home/default.nix
+++ home/default.nix
@@ -2,7 +2,5 @@
   imports = [./graphical ./theme.nix ./shell ./programs];
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "vscode"
-    ];
+    builtins.elem (lib.getName pkg) ["vscode"];
 }
