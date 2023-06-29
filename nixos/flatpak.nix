1dfe77e91675cc5f8b31971935a4e7cfaf384771
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-28 19:07:46 -0600

    flatpak: add systemd service to automatically update flatpaks every 4 hours

--- nixos/flatpak.nix
+++ nixos/flatpak.nix
@@ -1,3 +1,18 @@
-_: {
+{pkgs,lib,...}: {
   services.flatpak.enable = true;
+  systemd = {
+    services.flatpak-update = let
+      flatpak-wrap = args: "${lib.getExe pkgs.flatpak} ${args} --noninteractive --assumeyes";
+    in {
+      description = "Automatically update flatpaks";
+      startAt = "*:0/4:00";
+      serviceConfig.User = "kiri";
+      script = ''
+        echo "Starting updates..."
+        ${flatpak-wrap "update"}
+        ${flatpak-wrap "uninstall --unused"}
+        echo "Done!"
+      '';
+    };
+  };
 }
