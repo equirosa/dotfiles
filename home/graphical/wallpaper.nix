3a01fcfe3fb1b5aa94ddbcef1809f0980f9f3102
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-06-29 16:15:01 -0600

    hopefully fix wallpaper service

--- home/graphical/wallpaper.nix
+++ home/graphical/wallpaper.nix
@@ -1,13 +1,20 @@
-{pkgs, ...}: {
-  systemd.user.services.swww = let
-    swww = "${pkgs.swww}/bin/swww";
-  in {
-    Unit.Description = "Run swww init for graphical-session.target";
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  systemd.user.services.swww = {
+    Unit = {
+      After = ["graphical-session.target"];
+      Description = "Run swww init for graphical-session.target";
+      PartOf = ["graphical-session.target"];
+    };
     Install.WantedBy = ["graphical-session.target"];
     Service = {
-      Type = "simple";
-      ExecStart = "${swww}/bin/swww init && ${swww} img Pictures/desktop_backgrounds/gifs/city.gif";
-      Restart = "always";
+      ExecStart =
+        pkgs.writeShellScript "swww-init"
+        "${lib.getExe pkgs.swww} init";
+      Restart = "on-failure";
     };
   };
 }
