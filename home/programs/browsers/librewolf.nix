6fca41bb5dd6718b4a889ffa11b222eb1c7f924c
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-07-09 23:13:32 -0600

    librewolf: explicitly use the -wayland variant
    it's likely this isn't needed, but I'd rather have it

--- home/programs/browsers/librewolf.nix
+++ home/programs/browsers/librewolf.nix
@@ -1,6 +1,7 @@
-_: {
+{ pkgs, ... }: {
   programs.librewolf = {
     enable = true;
+    package = pkgs.librewolf-wayland;
     settings = {
       "identity.fxaccounts.enabled" = true;
       "privacy.resistFingerprinting.letterboxing" = true;
