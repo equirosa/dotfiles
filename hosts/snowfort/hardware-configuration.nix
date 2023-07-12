21453f0f77331f40d883fae5910c0ddad8c09d00
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-07-12 15:38:33 -0600

    snowfort: simplify hardware config

--- hosts/snowfort/hardware-configuration.nix
+++ hosts/snowfort/hardware-configuration.nix
@@ -19,26 +19,31 @@ in
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7c50c406-5a37-4bf8-bf44-48daf958e522";
-    fsType = "btrfs";
-    options = [ "subvol=nixos" ] ++ globalBtrfsOpts;
+
+  boot.initrd.luks.devices = {
+    "enc".device = "/dev/disk/by-uuid/10e5f1df-3228-478c-a505-2ec1861bc374";
+    "enc2".device = "/dev/disk/by-uuid/1e4df192-817e-4649-a884-44723fb34b46";
+    "enc-swap".device = "/dev/disk/by-uuid/42bb9c1c-1fd4-4a3e-9943-cf03d937d4b9";
+    "enc-nvme".device = "/dev/disk/by-uuid/ccccb02b-be56-4e7b-ba5a-eccd4d0ee6c9";
   };
 
-  boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/10e5f1df-3228-478c-a505-2ec1861bc374";
-  boot.initrd.luks.devices."enc2".device = "/dev/disk/by-uuid/1e4df192-817e-4649-a884-44723fb34b46";
-  boot.initrd.luks.devices."enc-swap".device = "/dev/disk/by-uuid/42bb9c1c-1fd4-4a3e-9943-cf03d937d4b9";
-  boot.initrd.luks.devices."enc-nvme".device = "/dev/disk/by-uuid/ccccb02b-be56-4e7b-ba5a-eccd4d0ee6c9";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/7c50c406-5a37-4bf8-bf44-48daf958e522";
+      fsType = "btrfs";
+      options = [ "subvol=nixos" ] ++ globalBtrfsOpts;
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/7c50c406-5a37-4bf8-bf44-48daf958e522";
-    fsType = "btrfs";
-    options = [ "subvol=home" ] ++ globalBtrfsOpts;
-  };
+    "/home" = {
+      device = "/dev/disk/by-uuid/7c50c406-5a37-4bf8-bf44-48daf958e522";
+      fsType = "btrfs";
+      options = [ "subvol=home" ] ++ globalBtrfsOpts;
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/DC5E-C2B3";
-    fsType = "vfat";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/DC5E-C2B3";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [{ device = "/dev/disk/by-uuid/3f464385-9f89-4cd1-8d48-fa8a3a156602"; }];
