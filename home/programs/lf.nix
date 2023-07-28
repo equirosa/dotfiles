eda74b3447edfff802db5d8ff8b12bf95b4beb12
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-07-27 19:05:45 -0600

    lf: merge all commands
    I'd likely remove all config anyway if I happen to remove zoxide,
    so trying to add conditionals will only complicate the code

--- home/programs/lf.nix
+++ home/programs/lf.nix
@@ -1,67 +1,55 @@
-{ config
-, pkgs
+{ pkgs
 , lib
 , ...
 }:
 let
-  inherit (lib) getExe recursiveUpdate;
-  inherit (lib.attrsets) optionalAttrs;
-  inherit (config.programs) zoxide;
-  zoxideCommands = {
-    zoxide = ''
-      %{{
-        result="$(zoxide query --exclude "''${PWD}" -- "$@")"
-        lf -remote "send ''${id} cd ''${result}"
-      }}
-    '';
-    zoxide_interactive = ''
-      ''${{
-        result="$(zoxide query -i -- "$@")"
-        lf -remote "send ''${id} cd ''${result}"
-      }}
-    '';
-  };
+  inherit (lib) getExe;
 in
 {
   programs = {
     lf = {
       enable = true;
       commands =
-        recursiveUpdate
-          {
-            on-cd = ''
-              &{{
-              printf "\033]0; $(pwd | sed "s|$HOME|~|") - lf\007" > /dev/tty
-              }}
-            '';
-            open = ''
-              ''${{for file in "$fx"; do
-              setsid xdg-open "$file" > /dev/null 2> /dev/null &
-              done}}
-            '';
-          }
-          (optionalAttrs
-            zoxide.enable
-            zoxideCommands);
-      keybindings =
-        recursiveUpdate
-          {
-            "<backspace2>" = ":set hidden!";
-            "<delete>" = "\$${pkgs.trash-cli}/bin/trash-put \"$fx\"";
-            "<enter>" = "push $";
-            D = "&${getExe pkgs.ripdrag} --all --and-exit \"$fx\"";
-            E = "push \$\${EDITOR}<space>";
-            L = "\$${getExe pkgs.lazygit}";
-            M = "push \$mkdir<space>-p<space>";
-            T = "push \$touch<space>";
-            e = "\$\${EDITOR} $fx";
-            U = ''umpv "$fx"'';
-            zx = "\$${getExe pkgs.archiver} unarchive \"$fx\"";
-          }
-          (optionalAttrs zoxide.enable {
-            zi = ":zoxide_interactive";
-            zz = "push :zoxide<space>";
-          });
+        {
+          on-cd = ''
+            &{{
+            printf "\033]0; $(pwd | sed "s|$HOME|~|") - lf\007" > /dev/tty
+            }}
+          '';
+          open = ''
+            ''${{for file in "$fx"; do
+            setsid xdg-open "$file" > /dev/null 2> /dev/null &
+            done}}
+          '';
+          zoxide = ''
+            %{{
+            result="$(zoxide query --exclude "''${PWD}" -- "$@")"
+            lf -remote "send ''${id} cd ''${result}"
+            }}
+          '';
+          zoxide_interactive = ''
+            ''${{
+            result="$(zoxide query -i -- "$@")"
+            lf -remote "send ''${id} cd ''${result}"
+            }}
+          '';
+        };
+      keybindings = {
+        "<backspace2>" = ":set hidden!";
+        "<delete>" = "\$${pkgs.trash-cli}/bin/trash-put \"$fx\"";
+        "<enter>" = "push $";
+        D = "&${getExe pkgs.ripdrag} --all --and-exit \"$fx\"";
+        E = "push \$\${EDITOR}<space>";
+        L = "\$${getExe pkgs.lazygit}";
+        M = "push \$mkdir<space>-p<space>";
+        T = "push \$touch<space>";
+        e = "\$\${EDITOR} $fx";
+        U = ''umpv "$fx"'';
+        zx = "\$${getExe pkgs.archiver} unarchive \"$fx\"";
+        # Zoxide
+        zi = ":zoxide_interactive";
+        zz = "push :zoxide<space>";
+      };
       previewer = {
         keybinding = "i";
         source = "${getExe pkgs.ctpv}";
