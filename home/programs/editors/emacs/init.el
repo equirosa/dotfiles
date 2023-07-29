14b5d1521a17e8d545fd1d9da8aa5ad86af0200c
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-07-29 00:52:40 -0600

    emacs: enable modus-vivendi-tinted

--- home/programs/editors/emacs/init.el
+++ home/programs/editors/emacs/init.el
@@ -207,6 +207,23 @@
 ;; ;; Flycheck
 ;; (use-package flycheck :config (global-flycheck-mode +1))
 
+;; Theme
+(use-package modus-themes
+  :ensure t
+  :config
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil)
+
+  ;; Maybe define some palette overrides, such as by using our presets
+  (setq modus-themes-common-palette-overrides
+        modus-themes-preset-overrides-intense)
+
+  ;; Load the theme of your choice.
+  (load-theme 'modus-vivendi-tinted)
+
+  (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
+
 ;; Org Mode
 (use-package org
   :hook ((org-mode . visual-line-mode)
