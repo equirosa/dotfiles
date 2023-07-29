bacdbfd6cf397586aec2baad3066686904cb284a
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-07-28 19:09:26 -0600

    regen: switch flake path to remote repo

--- home/programs/scripts/regen.sh
+++ home/programs/scripts/regen.sh
@@ -1,4 +1,4 @@
-flake_path="${HOME}/projects/dotfiles"
+flake_path="sourcehut:~eduardo_quiros/dotfiles"
 regen_nixos() {
 	sudo nixos-rebuild switch --flake "${flake_path}#$(hostname)"
 	notify-send "System Rebuilt!"
