81a9e965b03e1f7e6a8ce6b0fd91ad0deddda071
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-08-01 17:55:44 -0600

    emacs: remove rebinds in markdown-mode

--- home/programs/editors/emacs/init.el
+++ home/programs/editors/emacs/init.el
@@ -242,11 +242,8 @@
 
 ;; Markdown
 (use-package markdown-mode
-  :ensure t
   :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown")
-  :bind (:map markdown-mode-map
-         ("C-c C-e" . markdown-do)))
+  :custom (markdown-command "multimarkdown"))
 
 ;; Which-key
 (use-package which-key
