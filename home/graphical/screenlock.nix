e3ba060f75548f97c1bac642e8d03d282a9575ec
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-08-06 01:57:02 -0600

    swaylock: sort settings

--- home/graphical/screenlock.nix
+++ home/graphical/screenlock.nix
@@ -8,16 +8,16 @@ in
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
-      show-keyboard-layout = true;
+      clock = true;
       daemonize = true;
       effect-blur = "7x5";
-      clock = true;
+      fade-in = 0.2;
+      font-size = 25;
       image = "~/Pictures/desktop_backgrounds/gifs/city.gif";
       indicator = true;
-      font-size = 25;
       indicator-radius = 85;
       indicator-thickness = 16;
-      fade-in = 0.2;
+      show-keyboard-layout = true;
     };
   };
   services.swayidle = {
