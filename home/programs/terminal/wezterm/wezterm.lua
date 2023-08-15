194dda498c93b5d20ad9e027f0f9a7b98a377850
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-08-15 03:38:36 -0600

    wezterm: use new config

--- home/programs/terminal/wezterm/wezterm.lua
+++ home/programs/terminal/wezterm/wezterm.lua
@@ -1,5 +1,11 @@
-return {
-  font = wezterm.font("monospace"),
-  color_scheme = "Catppuccin Mocha",
-  window_background_opacity = 0.8,
-}
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("monospace")
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.8
+
+return config
