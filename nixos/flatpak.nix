34664c6d250f8e670d5e336d1a29004c8cdd8a10
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-08-19 16:08:42 -0600

    cleanup config

--- nixos/flatpak.nix
+++ nixos/flatpak.nix
@@ -1,22 +1,21 @@
 { pkgs
 , ...
-}: {
+}:
+let
+  flatwrap = args: "flatpak ${args} --noninteractive --assumeyes";
+in
+{
   services.flatpak.enable = true;
-  systemd = {
-    services.flatpak-update =
-      let
-        flatpak-wrap = args: "${pkgs.flatpak}/bin/flatpak ${args} --noninteractive --assumeyes";
-      in
-      {
-        description = "Automatically update flatpaks";
-        startAt = "*:0/4:00";
-        serviceConfig.User = "kiri";
-        script = ''
-          echo "Starting updates..."
-          ${flatpak-wrap "update"}
-          ${flatpak-wrap "uninstall --unused"}
-          echo "Done!"
-        '';
-      };
+  systemd.services.flatpak-update = {
+    description = "Automatically update flatpaks";
+    startAt = "*:0/4:00";
+    serviceConfig.User = "kiri";
+    path = [ pkgs.flatpak ];
+    script = ''
+      echo "Starting updates..."
+      ${flatwrap "update"}
+      ${flatwrap "uninstall --unused"}
+      echo "Done!"
+    '';
   };
 }
