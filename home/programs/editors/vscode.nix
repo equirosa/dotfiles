5769bb897e617274d406fa3376c10ff31349818a
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-03 01:18:07 -0600

    vscodium: add missing stuff for nix

--- home/programs/editors/vscode.nix
+++ home/programs/editors/vscode.nix
@@ -7,12 +7,18 @@
       bbenoist.nix
       catppuccin.catppuccin-vsc
       catppuccin.catppuccin-vsc-icons
+      jnoortheen.nix-ide
+      mkhl.direnv
     ];
     globalSnippets = { };
     userSettings = {
       "[nix]"."editor.tabSize" = 2;
       "editor.formatOnSave" = true;
       "files.autoSave" = "off";
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.iconTheme" = "catppuccin-mocha";
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nixd";
     };
   };
 }
