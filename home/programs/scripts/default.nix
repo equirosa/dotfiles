0da2e64daf265ef61d72267153d31c39a051acb6
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-13 14:00:22 -0600

    add nixpkgs search to search script

--- home/programs/scripts/default.nix
+++ home/programs/scripts/default.nix
@@ -220,7 +220,7 @@ in
     (writeShellApplication {
       name = "search";
       text = ''
-        search_options="farside.link/whoogle/search?q=\nyoutube.com/results?search_query=\ngithub.com/search?q=\nnixos.wiki/index.php?search=\nprotondb.com/search?q="
+        search_options="farside.link/whoogle/search?q=\nyoutube.com/results?search_query=\ngithub.com/search?q=\nnixos.wiki/index.php?search=\nprotondb.com/search?q=\nsearch.nixos.org/packages?query="
         search_site="$(echo -e "''${search_options}" | ${menu-program} --prompt-text "Search website")"
         input="$(${menu-program} --prompt-text "Search term")"
         librewolf "''${search_site}''${input}"
