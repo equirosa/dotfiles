d09c4b6ab1f6d59a1497be727ffe2fd5a90de6d0
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-14 23:36:03 -0600

    neovim: install nil to path

--- home/programs/editors/neovim/default.nix
+++ home/programs/editors/neovim/default.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }: {
   programs.neovim = {
     enable = true;
-    extraPackages = with pkgs;[ gcc gnumake ];
+    extraPackages = with pkgs; [
+      gcc
+      gnumake
+      # LSPs
+      nil
+    ];
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
