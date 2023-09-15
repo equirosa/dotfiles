25a46edae92f9486d0e5b9e6fe13dff8fc10ba7d
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-14 22:25:06 -0600

    trim fonts

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -2,7 +2,7 @@
   fonts = {
     fontconfig = {
       enable = true;
-      defaultFonts.monospace = [ "Iosevka Comfy" "Iosevka Comfy Duo" "FiraCode Nerd Font" ];
+      defaultFonts.monospace = [ "Iosevka Comfy" "FiraCode Nerd Font" ];
     };
     fontDir.enable = true;
     packages = with pkgs; [
@@ -10,7 +10,6 @@
       emacs-all-the-icons-fonts
       font-awesome
       iosevka-comfy.comfy
-      iosevka-comfy.comfy-duo
       liberation_ttf
       nerdfonts
       noto-fonts-emoji
