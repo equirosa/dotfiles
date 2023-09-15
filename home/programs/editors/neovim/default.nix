edc6188bb401cdcb82078f8cba8650f5418c8c35
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-14 22:55:36 -0600

    neovim: reorder

--- home/programs/editors/neovim/default.nix
+++ home/programs/editors/neovim/default.nix
@@ -1,5 +1,13 @@
 { pkgs, lib, ... }: {
   programs.neovim = {
     enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+  xdg.configFile."nvim" = {
+    source = ./config;
+    # onChange = "";
+    recursive = true;
   };
 }
