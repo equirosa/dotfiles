2c7df8af30595146b5cf5641a941b8ca2e09e2d7
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-21 14:55:51 -0600

    move substituters config to flake
    having it in the gaming module is a little bit awkward, since a good
    chunk of substituters don't have anything to do with gaming at
    all. I'll store them in `flake.nix` for now, but probably will move
    them to their own file in the future.

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -40,19 +40,6 @@ in
     };
     steam-hardware.enable = true;
   };
-  nix.settings = {
-    substituters = [
-      "https://nix-gaming.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://cache.nixos.org/"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
   nixpkgs = {
     config = {
       allowUnfreePredicate = pkg:
