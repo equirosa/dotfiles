7879a59679dde5f9feeee77350764a21ee22c902
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-27 00:00:09 -0600

    xdg-open: handle only one argument

--- home/programs/scripts/xdg-open.sh
+++ home/programs/scripts/xdg-open.sh
@@ -1,16 +1,14 @@
 [ $# -eq 0 ] && notify-send "No arguments provided. Exitting..." && exit 1
-
+arg=$1
 add_torrent() {
     setsid transmission-remote -a "$1" && notify-send -u low "Torrent Added! ✅"
 }
 
-for arg in "$@"; do
-    case "${arg}" in
-        magnet* | *.torrent ) add_torrent "$arg" ;;
-        *.org ) setsid emacsclient --create-frame "${arg}" ;;
-        *.png | *.jpg | *.jpeg | *.webp ) setsid imv "${arg}" ;;
-        *.pdf ) zathura "$arg" ;;
-        http* ) firefox -P default "${arg}" ;;
-        * ) setsid xdg-open "${arg}" ;;
-    esac
-done
+case "${arg}" in
+    magnet* | *.torrent ) add_torrent "$arg" ;;
+    *.org ) setsid emacsclient --create-frame "${arg}" ;;
+    *.png | *.jpg | *.jpeg | *.webp ) setsid imv "${arg}" ;;
+    *.pdf ) zathura "$arg" ;;
+    http* ) firefox -P default "${arg}" ;;
+    * ) setsid xdg-open "${arg}" ;;
+esac
