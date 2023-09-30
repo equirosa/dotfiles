f0bc31c6029582f52c7bb6fcfa881eff874f6c6b
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-09-30 16:25:45 -0600

    yazi: add a fallback opener

--- home/programs/file-manager.nix
+++ home/programs/file-manager.nix
@@ -251,16 +251,22 @@ in
     };
     settings = {
       manager.show_hidden = false;
-      opener = {
-        image = [
-          { exec = ''xdg-open "$@"''; display_name = "Open"; }
-          {
-            exec = ''${exiftool} "$1"; echo "Press enter to exit"; read'';
-            block = true;
-            display_name = "Show EXIF";
-          }
-        ];
-      };
+      opener =
+        let
+          defaultOpener =
+            { exec = ''xdg-open "$@"''; display_name = "Open"; };
+        in
+        {
+          fallback = [ defaultOpener ];
+          image = [
+            defaultOpener
+            {
+              exec = ''${exiftool} "$1"; echo "Press enter to exit"; read'';
+              block = true;
+              display_name = "Show EXIF";
+            }
+          ];
+        };
     };
     theme = { };
   };
