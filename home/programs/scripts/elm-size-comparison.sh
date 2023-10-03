86ace7998dde872d76676d50dac08851c81cd10a
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-10-03 14:33:42 -0600

    fix script

--- home/programs/scripts/elm-size-comparison.sh
+++ home/programs/scripts/elm-size-comparison.sh
@@ -1,10 +1,10 @@
 js="$(mktemp -t elm.js.XXXXXX)"
 min="$(mktemp -t elm.min.js.XXXXXX)"
 
-elm make --optimize --output=$js $@
+elm make --optimize --output="$js" "$@"
 
-uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+uglifyjs "$js" --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output "$min"
 
-echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
-echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
-echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+echo "Initial size: $(wc -c "$js") bytes  ($js)"
+echo "Minified size:$(wc -c < "$min") bytes  ($min)"
+echo "Gzipped size: $(gzip -c < "$min" | wc -c) bytes"
