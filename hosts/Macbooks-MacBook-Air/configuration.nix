7f556fd3bb4a05b284998ec360b19be36361bfca
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-10-20 15:06:16 -0600

    add more darwin stuff

--- hosts/Macbooks-MacBook-Air/configuration.nix
+++ hosts/Macbooks-MacBook-Air/configuration.nix
@@ -1,4 +1,17 @@
-_: {
+{ pkgs, ... }: {
   homebrew = { enable = true; };
+  nix = {
+    extraOptions = ''
+      experimental-features = flakes nix-command recursive-nix
+      keep-outputs = true
+      keep-derivations = true
+    '';
+    settings = {
+      auto-optimise-store = true;
+      sandbox = true;
+    };
+  };
+  users.users.eduardo.packages = with pkgs; [ direnv wormhole-rs ];
   nixpkgs.hostPlatform = "x86_64-darwin";
+  services.nix-daemon.enable = true;
 }
