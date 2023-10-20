fb9cdd000ea924e4909f54973fb3c2ae56dc2fc4
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-10-20 15:06:40 -0600

    integrate home-manager to nix-darwin

--- flake.nix
+++ flake.nix
@@ -115,7 +115,24 @@
       };
       darwinConfigurations.Macbooks-MacBook-Air = nix-darwin.lib.darwinSystem {
         system = "x86_64-darwin";
-        modules = [ ./hosts/Macbooks-MacBook-Air/configuration.nix ];
+        modules = [ ./hosts/Macbooks-MacBook-Air/configuration.nix
+        home-manager.darwinModules.home-manager {
+                        home-manager = {
+                extraSpecialArgs = { inherit colors inputs wrapper-manager overlays; };
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.kiri = { osConfig, ... }: {
+                  imports = [
+                    ./home/programs/editors/neovim
+                    nix-index-database.hmModules.nix-index
+                    nixvim.homeManagerModules.nixvim
+                  ];
+                  home.stateVersion = osConfig.system.stateVersion;
+                };
+              };
+
+        }
+         ];
       };
     };
 }
