4eed4f3a549b39aeaee012cc4ff716730923fc7b
Eduardo Quiros <kiri@MacBook-Air-de-Eduardo.local>
2023-10-22 03:00:45 -0600

    wezterm: don't try to load monospace

--- home/programs/terminal/wezterm/wezterm.lua
+++ home/programs/terminal/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("monospace")
+-- config.font = wezterm.font("monospace")
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.8
 
