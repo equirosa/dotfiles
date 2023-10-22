80cb5290af5247a1f9a2b6478dc6beede8b364be
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-10-22 05:42:18 -0600

    git: don't sign by default
    I currently don't have my key here

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -28,7 +28,7 @@ _: {
       ignores = [ "*~" "*.swp" ".direnv" ".DS_Store" ];
       signing = {
         key = "03678E9642EB6D9E99974ACFB77F36C3F12720B4";
-        signByDefault = true;
+        # signByDefault = true;
       };
       userEmail = "eduardo@eduardoquiros.com";
       userName = "Eduardo Quiros";
