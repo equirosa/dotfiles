b0c930918c8ee80cf74186e7533f4d014cf81735
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-06 16:09:15 -0600

    darwin: install fonts

--- hosts/Macbooks-MacBook-Air/configuration.nix
+++ hosts/Macbooks-MacBook-Air/configuration.nix
@@ -8,4 +8,16 @@
   users.users.kiri.packages = with pkgs; [ direnv git lazygit ];
   programs.zsh.enable = true;
   services.nix-daemon.enable = true;
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      dejavu_fonts
+      emacs-all-the-icons-fonts
+      font-awesome
+      iosevka-comfy.comfy
+      liberation_ttf
+      nerdfonts
+      noto-fonts-emoji
+    ];
+  };
 }
