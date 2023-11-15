a101a049d8a886b918f29b2a01606961a4658eb7
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-14 20:14:55 -0600

    set nix to use internal pkgs registry

--- hosts/global.nix
+++ hosts/global.nix
@@ -1,5 +1,11 @@
-_: {
+{ pkgs, ... }: {
   nix = {
+    registry = {
+      nixpkgs.to = {
+        type = "path";
+        path = pkgs.path;
+      };
+    };
     extraOptions = ''
       experimental-features = flakes nix-command recursive-nix
       keep-outputs = true
