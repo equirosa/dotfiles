4f40a9c59c4bd92826c3d3b6f24d6da4b1e247d3
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-15 18:43:00 -0600

    kitty: use fancy tab style

--- home/programs/terminal/kitty.nix
+++ home/programs/terminal/kitty.nix
@@ -6,7 +6,14 @@
       size = 14;
     };
     keybindings = { };
-    settings = { background_opacity = "${colors.opacity}"; };
+    settings = {
+      background_opacity = "${colors.opacity}";
+      tab_bar_min_tabs = 1;
+      tab_bar_edge = "bottom";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+    };
     theme = "Gruvbox Dark Hard";
   };
 }
