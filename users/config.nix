3b104f3b200c857e23e58d6d0b79eb95e8c231e0
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-26 14:08:47 -0600

    add users directory

---
+++ users/config.nix
@@ -0,0 +1,10 @@
+{
+  myself = "kiri";
+  users = {
+    kiri = {
+      name = "Eduardo Quiros";
+      email = "eduardo@eduardoquiros.com";
+      sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEy17h0xv3f2RP6BGtlfByjMMSwoQ0jvUiEGKKqN/uyA snowfort" ];
+    };
+  };
+}
