5058d47d360429e50ea52dce7c3130e029ffca77
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-27 20:22:04 -0600

    more segmentation

--- home/default.nix
+++ home/default.nix
@@ -1,20 +1,38 @@
-{ self, lib, ... }: {
+{ self, lib, inputs, ... }: {
   flake = {
     homeModules = {
       common = {
-        imports = [ ./shell ./programs ];
+        imports = [
+          inputs.nix-index-database.hmModules.nix-index
+          ./programs
+          ./shell
+        ];
         programs = {
           nix-index-database.comma.enable = true;
           nix-index.enable = true;
+        };
+        home-manager = {
+          backupFileExtension = "hmBackup";
+          useGlobalPkgs = true;
+          useUserPackages = true;
         };
         nixpkgs.config.allowUnfreePredicate = pkg:
           builtins.elem (lib.getName pkg) [ "vscode" ];
       };
       linux = {
-        imports = [ self.homeModules.common ];
+        imports = [
+          self.homeModules.common
+          inputs.nix-colors.homeManagerModules.default
+          ./linux.nix
+        ];
       };
       darwin = {
         imports = [ self.homeModules.common ];
+        home-manager.home = {
+          username = "kiri";
+          homeDirectory = lib.mkForce "/Users/kiri";
+          stateVersion = "23.11";
+        };
       };
     };
   };
