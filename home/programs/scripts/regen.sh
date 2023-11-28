ba56ace758ad25d8c70a982c90d5717b72a462ae
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-28 01:39:05 -0600

    run nix fmt

--- home/programs/scripts/regen.sh
+++ home/programs/scripts/regen.sh
@@ -1,43 +1,43 @@
 flake_path="${HOME}/projects/dotfiles"
 platform="$(uname -s)"
 regen_nixos() {
-	case "$platform" in
-		"Linux")
-			sudo nixos-rebuild switch --flake "${flake_path}"
-			notify-send "System Rebuilt!"
-			;;
-		"Darwin")
-			darwin-rebuild switch --flake "${flake_path}"
-			;;
-		*)
-			echo "PLATFORM UNKNOWN"
-			exit 1
-			;;
-	esac
+  case "$platform" in
+  "Linux")
+    sudo nixos-rebuild switch --flake "${flake_path}"
+    notify-send "System Rebuilt!"
+    ;;
+  "Darwin")
+    darwin-rebuild switch --flake "${flake_path}"
+    ;;
+  *)
+    echo "PLATFORM UNKNOWN"
+    exit 1
+    ;;
+  esac
 }
 
 test_run() {
-	cd "$(mktemp -d)"
-	case "$platform" in
-		"Linux")
-			nixos-rebuild build --flake "${flake_path}"
-			;;
-		"Darwin")
-			darwin-rebuild build --flake "${flake_path}"
-			;;
-		*)
-			echo "PLATFORM UNKNOWN"
-			exit 1
-			;;
-	esac
-	nvd diff /run/current-system/ ./result
+  cd "$(mktemp -d)"
+  case "$platform" in
+  "Linux")
+    nixos-rebuild build --flake "${flake_path}"
+    ;;
+  "Darwin")
+    darwin-rebuild build --flake "${flake_path}"
+    ;;
+  *)
+    echo "PLATFORM UNKNOWN"
+    exit 1
+    ;;
+  esac
+  nvd diff /run/current-system/ ./result
 }
 
 if [ $# -eq 0 ]; then
-	regen_nixos
+  regen_nixos
 else
-	case "${1}" in
-		"test") test_run ;;
-		"os" | *) regen_nixos ;;
-	esac
+  case "${1}" in
+  "test") test_run ;;
+  "os" | *) regen_nixos ;;
+  esac
 fi
--- home/programs/scripts/run-backups.sh
+++ home/programs/scripts/run-backups.sh
@@ -6,44 +6,44 @@ LGREEN="\033[1;32m"
 NC="\033[0m"
 
 print_success() {
-	printf "%b%s%b\n" "$LGREEN" "${1}" "$NC"
+  printf "%b%s%b\n" "$LGREEN" "${1}" "$NC"
 }
 
 print_announce() {
-	printf "%b%s%b\n" "$LBLUE" "${1}" "$NC"
+  printf "%b%s%b\n" "$LBLUE" "${1}" "$NC"
 }
 
 print_announce "Starting Backup to BorgBase"
 borg create --progress --verbose --stats --checkpoint-interval 600 hvwib450@hvwib450.repo.borgbase.com:repo::"$BORG_BACKUP_FORMAT" \
-	~/Documents/ \
-	~/Downloads/ \
-	~/Games/itch/ \
-	~/Music/ \
-	~/Sync/ \
-	~/Pictures/ \
-	~/Templates/ \
-	~/Videos/ \
-	~/projects \
-	~/.local/share/libvirt/ \
-	--exclude "${HOME}/projects/nixpkgs/" \
-	--exclude "*/league-of-legends/*" \
-	--exclude "*/node_modules/*" \
-	--exclude "*/oses/*" \
-	--exclude '*/.stversions' \
-	--exclude '*/.stfolder' \
-	--exclude '*/battlenet' \
-	--exclude '*/epic' \
-	--exclude '*/rocket-league' \
-	--exclude "*/.direnv/" \
-	--exclude "*/.git/" \
-	--exclude "*/.var/" \
-	--exclude "*/target/" \
-	--exclude "*/torrented" \
-	--exclude "*/unhidden" \
-	--exclude "*/plain" \
-	--exclude "*/.thumbnails" \
-	--compression auto,zstd,10 \
-	&& print_announce "PRUNING" \
-	&& borg prune --progress --verbose --stats --save-space --keep-daily=7 --keep-monthly=-1 --keep-weekly=4 --keep-within=1d \
-		hvwib450@hvwib450.repo.borgbase.com:repo \
-	&& print_success "BACKUP TO BORGBASE SUCCESSFUL!"
+  ~/Documents/ \
+  ~/Downloads/ \
+  ~/Games/itch/ \
+  ~/Music/ \
+  ~/Sync/ \
+  ~/Pictures/ \
+  ~/Templates/ \
+  ~/Videos/ \
+  ~/projects \
+  ~/.local/share/libvirt/ \
+  --exclude "${HOME}/projects/nixpkgs/" \
+  --exclude "*/league-of-legends/*" \
+  --exclude "*/node_modules/*" \
+  --exclude "*/oses/*" \
+  --exclude '*/.stversions' \
+  --exclude '*/.stfolder' \
+  --exclude '*/battlenet' \
+  --exclude '*/epic' \
+  --exclude '*/rocket-league' \
+  --exclude "*/.direnv/" \
+  --exclude "*/.git/" \
+  --exclude "*/.var/" \
+  --exclude "*/target/" \
+  --exclude "*/torrented" \
+  --exclude "*/unhidden" \
+  --exclude "*/plain" \
+  --exclude "*/.thumbnails" \
+  --compression auto,zstd,10 &&
+  print_announce "PRUNING" &&
+  borg prune --progress --verbose --stats --save-space --keep-daily=7 --keep-monthly=-1 --keep-weekly=4 --keep-within=1d \
+    hvwib450@hvwib450.repo.borgbase.com:repo &&
+  print_success "BACKUP TO BORGBASE SUCCESSFUL!"
