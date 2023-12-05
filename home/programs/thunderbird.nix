be1c4537ac33e7663916476273652c3e9dba3093
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-12-05 17:52:34 -0600

    enable beeper systemd service

--- home/programs/thunderbird.nix
+++ home/programs/thunderbird.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }: {
+  systemd.user.services.beeper = {
+    Unit = {
+      Description = "Run Beeper";
+      After = [ "network.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.beeper}/bin/beeper";
+      Restart = "always";
+      RestartSec = 3;
+    };
+    Install = {
+      WantedBy = [ "graphical.target" ];
+    };
+  };
   systemd.user.services.thunderbird = {
     Unit = {
       Description = "Run Thunderbird";
