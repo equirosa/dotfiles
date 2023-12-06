ebca8a6e75089ca66af8694c4bf2f6951ba1e5d4
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-12-05 18:15:11 -0600

    switch out dnscrypt-proxy with resolved

--- nixos/global.nix
+++ nixos/global.nix
@@ -35,16 +35,17 @@
   security.pam.services.login.gnupg.enable = true;
   services = {
     dbus.packages = [ pkgs.gcr ];
-    dnscrypt-proxy2 = {
+    resolved = {
       enable = true;
-      settings = {
-        sources.public-resolvers = {
-          urls = [ "https://download.dnscrypt.info/resolvers-list/v2/public-resolvers.md" ];
-          cache_file = "public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-          refresh_delay = 72;
-        };
-      };
+      dnssec = "true";
+      extraConfig = ''
+        [Resolve]
+        DNS=45.90.28.0#149cea.dns.nextdns.io
+        DNS=2a07:a8c0::#149cea.dns.nextdns.io
+        DNS=45.90.30.0#149cea.dns.nextdns.io
+        DNS=2a07:a8c1::#149cea.dns.nextdns.io
+        DNSOverTLS=yes
+      '';
     };
     pipewire = {
       enable = true;
