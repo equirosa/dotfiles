48ba84e1b243a3469f9d4b3c770243459bb738b6
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-12-11 20:26:41 -0600

    add agenix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Kiri's Nix configuration";
 
   inputs = {
+    agenix.url = "github:ryantm/agenix";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
@@ -30,6 +31,7 @@
 
   outputs =
     inputs@{ self
+    , agenix
     , flake-utils
     , home-manager
     , nix-colors
@@ -80,71 +82,78 @@
         };
       })
     // {
-      nixosConfigurations.snowfort = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          {
-            programs.hyprland.enable = true;
-            security.pam.services.swaylock = { };
-            nixpkgs.overlays = overlays;
-            nix.settings = {
-              builders-use-substitutes = true;
-              substituters = [
-                "https://nix-gaming.cachix.org"
-                "https://nix-community.cachix.org"
-                "https://hyprland.cachix.org"
-                "https://cache.nixos.org/"
-              ];
-              trusted-public-keys = [
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-              ];
-            };
-          }
-          ./hosts/snowfort/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = common-hm-config // {
-              extraSpecialArgs = { inherit colors inputs; };
-              users.kiri = { osConfig, ... }: {
-                imports = [
-                  ./home/linux.nix
-                  nix-index-database.hmModules.nix-index
-                  inputs.nixvim.homeManagerModules.nixvim
-                  nix-colors.homeManagerModules.default
+      nixosConfigurations.snowfort =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            agenix.nixosModules.default
+            {
+              programs.hyprland.enable = true;
+              security.pam.services.swaylock = { };
+              nixpkgs.overlays = overlays;
+              nix.settings = {
+                builders-use-substitutes = true;
+                substituters = [
+                  "https://nix-gaming.cachix.org"
+                  "https://nix-community.cachix.org"
+                  "https://hyprland.cachix.org"
+                  "https://cache.nixos.org/"
                 ];
-                colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
-                home.stateVersion = osConfig.system.stateVersion;
+                trusted-public-keys = [
+                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                  "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+                  "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                ];
               };
-            };
-          }
-        ];
-        specialArgs = { inherit colors inputs; };
-      };
-      darwinConfigurations.MacBook-Air-de-Eduardo = nix-darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = [
-          ./hosts/Macbooks-MacBook-Air/configuration.nix
-          { nixpkgs.overlays = overlays; }
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = common-hm-config // {
-              extraSpecialArgs = { inherit colors inputs; };
-              users.kiri = { lib, ... }: {
-                imports = [
-                  nix-index-database.hmModules.nix-index
-                  ./home
-                ];
-                home = {
-                  username = "kiri";
-                  homeDirectory = lib.mkForce "/Users/kiri";
-                  stateVersion = "23.11";
+            }
+            ./hosts/snowfort/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = common-hm-config // {
+                extraSpecialArgs = { inherit colors inputs; };
+                users.kiri = { osConfig, ... }: {
+                  imports = [
+                    ./home/linux.nix
+                    nix-index-database.hmModules.nix-index
+                    inputs.nixvim.homeManagerModules.nixvim
+                    nix-colors.homeManagerModules.default
+                  ];
+                  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+                  home.stateVersion = osConfig.system.stateVersion;
+                  home.packages = [ agenix.packages.${system}.default ];
                 };
               };
-            };
-          }
-        ];
-      };
+            }
+          ];
+          specialArgs = { inherit colors inputs; };
+        };
+      darwinConfigurations.MacBook-Air-de-Eduardo = nix-darwin.lib.darwinSystem
+        {
+          system = "x86_64-darwin";
+          modules = [
+            ./hosts/Macbooks-MacBook-Air/configuration.nix
+            { nixpkgs.overlays = overlays; }
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = common-hm-config // {
+                extraSpecialArgs = { inherit colors inputs; };
+                users.kiri = { lib, ... }: {
+                  imports = [
+                    nix-index-database.hmModules.nix-index
+                    ./home
+                  ];
+                  home = {
+                    username = "kiri";
+                    homeDirectory = lib.mkForce "/Users/kiri";
+                    stateVersion = "23.11";
+                  };
+                };
+              };
+            }
+          ];
+        };
     };
 }
