794c242fe839ca155d188240cc40d602fc734dbb
kiri <eduardo@eduardoquiros.com>
2023-12-21 08:09:17 +0000

    keep stable flake in case I need to downgrade a package

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Kiri's Nix configs";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     # If you're going to use darwinConfigurations uncomment next two inputs
     # Otherwise you can remove them
