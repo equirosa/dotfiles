6faf9c0faa0ae438073ae6d75c1d08841b3a22e4
kiri <eduardo@eduardoquiros.com>
2023-12-21 15:08:58 -0600

    remove unused code

--- nixos-configurations/snowfort.nix
+++ nixos-configurations/snowfort.nix
@@ -29,25 +29,6 @@
   services.xserver.xkb.layout = "us,latam";
   services.xserver.xkb.options = "grp:win_space_toggle";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
