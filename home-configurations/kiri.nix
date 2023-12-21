b91024aa5c92cd44a58a20c3999719b78bd3889d
kiri <eduardo@eduardoquiros.com>
2023-12-21 16:48:18 -0600

    remove conflicting declaration

--- home-configurations/kiri.nix
+++ home-configurations/kiri.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
-let
-  inherit (pkgs.stdenv) isDarwin;
-in
 {
   home = {
     username = "kiri";
     stateVersion = "23.11";
-    homeDirectory = if isDarwin then "/Users/kiri" else "/home/kiri/";
   };
 }
