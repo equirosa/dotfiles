946cd7ba19037746505eaabb255228db0946a875
kiri <eduardo@eduardoquiros.com>
2023-12-26 00:34:13 -0600

    integrate older scripts

--- home-modules/scripts/default.nix
+++ home-modules/scripts/default.nix
@@ -10,8 +10,17 @@ in
   home.packages = [
     (writeShellApplication {
       name = "regen";
-      runtimeInputs = attrValues { inherit (pkgs) nvd; };
+      runtimeInputs = attrValues { inherit (pkgs) libnotify nvd; };
       text = fileContents ./regen.sh;
+    })
+    (writeShellApplication {
+      name = "show-nix-store-path";
+      text = ''realpath "$(command -v "''${1}")"'';
+    })
+    (writeShellApplication {
+      name = "show-script";
+      runtimeInputs = attrValues { inherit (pkgs) bat; };
+      text = ''bat -p "$(show-nix-store-path "''${1}")"'';
     })
   ];
 }
