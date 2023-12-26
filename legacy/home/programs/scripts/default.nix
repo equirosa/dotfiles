ebd2cf5aa8d82fa64d4e7d9d8df5652d597c4802
kiri <eduardo@eduardoquiros.com>
2023-12-26 00:28:53 -0600

    remove unused code

--- legacy/home/programs/scripts/default.nix
+++ legacy/home/programs/scripts/default.nix
@@ -1,15 +1,5 @@
-{ pkgs, lib, ... }:
-let
-  inherit (lib) fileContents;
-  inherit (import ../../shell/aliases.nix { inherit pkgs lib; }) cat;
-in
-{
+{ pkgs, ... }: {
   home.packages = with pkgs; [
-    (writeShellApplication {
-      name = "regen";
-      runtimeInputs = [ pkgs.nvd ];
-      text = fileContents ./regen.sh;
-    })
     (writeShellApplication {
       name = "show-nix-store-path";
       text = ''realpath "$(command -v "''${1}")"'';
