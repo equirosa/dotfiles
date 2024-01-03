359967c7d6ddd7592e3c4b1d4ad91a0b6cff4e3e
Eduardo Quiros <eduardo@eduardoquiros.com>
2024-01-02 20:18:05 -0600

    neovim: remove unused locals

--- home-modules/neovim/config/init.lua
+++ home-modules/neovim/config/init.lua
@@ -4,13 +4,7 @@ else
 	require("config.lazy")
 
 	local ls = require("luasnip")
-	local s = ls.snippet
 	local ps = ls.parser.parse_snippet
-	local t = ls.text_node
-	local i = ls.insert_node
-	local extras = require("luasnip.extras")
-	local rep = extras.rep
-	local fmt = require("luasnip.extras.fmt").fmt
 
 	ls.add_snippets("rust", {
 		ps("letsig", "let ($1, set_$1) = create_signal($0);"),
