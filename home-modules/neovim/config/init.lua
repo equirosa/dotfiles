5a12413846530b30c2fbe9a15664c984d1f11ca3
Eduardo Quiros <eduardo@eduardoquiros.com>
2024-01-02 20:17:02 -0600

    neovim: conditionally load lazyvim if not inside vscode

--- home-modules/neovim/config/init.lua
+++ home-modules/neovim/config/init.lua
@@ -1,29 +1,31 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+else
+	-- bootstrap lazy.nvim, LazyVim and your plugins
+	require("config.lazy")
 
-local ls = require("luasnip")
-local s = ls.snippet
-local ps = ls.parser.parse_snippet
-local t = ls.text_node
-local i = ls.insert_node
-local extras = require("luasnip.extras")
-local rep = extras.rep
-local fmt = require("luasnip.extras.fmt").fmt
+	local ls = require("luasnip")
+	local s = ls.snippet
+	local ps = ls.parser.parse_snippet
+	local t = ls.text_node
+	local i = ls.insert_node
+	local extras = require("luasnip.extras")
+	local rep = extras.rep
+	local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("rust", {
-	ps("letsig", "let ($1, set_$1) = create_signal($0);"),
-})
+	ls.add_snippets("rust", {
+		ps("letsig", "let ($1, set_$1) = create_signal($0);"),
+	})
 
-ls.add_snippets("nix", {
-	ps("enable", "enable = true;$0"),
-	ps("bool", "$1 = true;$0"),
-	ps("list", "$1 = [$2];$0"),
-	ps("set", "$1 = {\n$2\n};$0"),
-	ps("str", '$1 = "$2";$0'),
-	ps("mls", "$1 = ''\n$2\n'';$0"),
-	ps(
-		"hm-systemd-service",
-		[[
+	ls.add_snippets("nix", {
+		ps("enable", "enable = true;$0"),
+		ps("bool", "$1 = true;$0"),
+		ps("list", "$1 = [$2];$0"),
+		ps("set", "$1 = {\n$2\n};$0"),
+		ps("str", '$1 = "$2";$0'),
+		ps("mls", "$1 = ''\n$2\n'';$0"),
+		ps(
+			"hm-systemd-service",
+			[[
     systemd.user.services.$1 = {
     Unit = {
       Description = "$2";
@@ -39,10 +41,10 @@ ls.add_snippets("nix", {
         };
       };$0
     ]]
-	),
-	ps(
-		"wrapped-package",
-		[[
+		),
+		ps(
+			"wrapped-package",
+			[[
     package = inputs.wrapper-manager.lib.build {
       inherit pkgs;
       modules = [{
@@ -53,5 +55,6 @@ ls.add_snippets("nix", {
       }];
     };$0
     ]]
-	),
-})
+		),
+	})
+end
